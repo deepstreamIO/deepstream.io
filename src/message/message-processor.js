a409cfb7e532d1fc7f36387b6fce3d6ae95cc525
hoxton-one <info@golden-layout.com>
2014-11-17 17:03:38 +0000

    Added comments to message processor

--- src/message/message-processor.js
+++ src/message/message-processor.js
@@ -1,12 +1,46 @@
 var messageParser = require( './message-parser' ),
 	C = require( '../constants/constants' );
 
+/**
+ * The MessageProcessor consumes blocks of parsed messages emitted by the
+ * ConnectionEndpoint, checks if they are permissioned and - if they
+ * are - forwards them.
+ *
+ * @constructor
+ * 
+ * @param {Object} options deepstream options
+ */
 var MessageProcessor = function( options ) {
 	this._options = options;
 };
 
+/**
+ * There will only ever be one consumer of forwarded messages. So rather than using
+ * events - and their performance overhead - the messageProcessor exposes
+ * this method that's expected to be overwritten.
+ *
+ * @param   {SocketWrapper} socketWrapper
+ * @param   {Object} message the parsed message
+ *
+ * @overwrite
+ *
+ * @returns {void}
+ */
 MessageProcessor.prototype.onAuthenticatedMessage = function( socketWrapper, message ){};
 
+/**
+ * This method is the way the message processor accepts input. It receives arrays
+ * of parsed messages, iterates through them and issues permission requests for
+ * each individual message
+ *
+ * @todo The responses from the permissionHandler might arive in any arbitrary order - order them
+ * @todo Handle permission handler timeouts
+ *
+ * @param   {SocketWrapper} socketWrapper
+ * @param   {Object} message parsed message
+ *
+ * @returns {void}
+ */
 MessageProcessor.prototype.process = function( socketWrapper, message ) {
 	var parsedMessages = messageParser.parse( message ),
 		i;
@@ -26,6 +60,18 @@ MessageProcessor.prototype.process = function( socketWrapper, message ) {
 	}
 };
 
+/**
+ * Processes the response that's returned by the permissionHandler.
+ *
+ * @param   {SocketWrapper} 	socketWrapper
+ * @param   {Object} message 	parsed message - might have been manipulated
+ *                            	by the permissionHandler
+ * @param   {Error} error 		error or null if no error. Denied permissions will be expressed
+ *                          	by setting result to false
+ * @param   {Boolean} result    true if permissioned
+ *
+ * @returns {void}
+ */
 MessageProcessor.prototype._onPermissionResponse = function( socketWrapper, message, error, result ) {
 	if( error !== null ) {
 		socketWrapper.sendError( message.topic, C.EVENT.MESSAGE_PERMISSION_ERROR, error.toString() );
