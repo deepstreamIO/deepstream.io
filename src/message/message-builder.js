70ac37b4629c86bcc8748d039c17785ee5df792a
hoxton-one <info@golden-layout.com>
2014-11-17 17:10:24 +0000

    Added comments to message builder

--- src/message/message-builder.js
+++ src/message/message-builder.js
@@ -1,10 +1,16 @@
 var constants = require( '../constants/constants' ),
 	SEP = constants.MESSAGE_PART_SEPERATOR;
 
-exports.getErrorMsg = function( topic, type, message ) {
-	return topic + SEP + 'E' + SEP + type + SEP + message;
-};
-
+/**
+ * Creates a deepstream message string, based on the 
+ * provided parameters
+ *
+ * @param   {String} topic  One of CONSTANTS.TOPIC
+ * @param   {String} action One of CONSTANTS.ACTIONS
+ * @param   {Array} data An array of strings or JSON-serializable objects
+ *
+ * @returns {String} deepstream message string
+ */
 exports.getMsg = function( topic, action, data ) {
 	var sendData = [ topic, action ],
 		i;
@@ -21,3 +27,20 @@ exports.getMsg = function( topic, action, data ) {
 
 	return sendData.join( SEP );
 };
+
+/**
+ * Creates a deepstream error message string based on the provided
+ * arguments
+ *
+ * @param   {String} topic   One of CONSTANTS.TOPIC - error messages might either be send on
+ *                           the generic ERROR topic or on the topic that caused the error
+ *                           
+ * @param   {String} type    One of CONSTANTS.EVENT
+ * @param   {String} message A generic error message
+ *
+ * @returns {String} deepstream error message string
+ */
+exports.getErrorMsg = function( topic, type, message ) {
+	return topic + SEP + 'E' + SEP + type + SEP + message;
+};
+
