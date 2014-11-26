e012c3d7559cd1cabcc2527002221d6af869779f
hoxton_one <info@golden-layout.com>
2014-11-26 11:50:26 +0000

    remote rpc provider registry tests

--- test/mocks/message-connector-mock.js
+++ test/mocks/message-connector-mock.js
@@ -1,7 +1,10 @@
+var EventEmitter = require( 'events' ).EventEmitter;
+
 var MessageConnectorMock = function() {
     this.lastPublishedTopic = null;
     this.lastPublishedMessage = null;
     this.lastSubscribedTopic = null;
+    this._eventEmitter = new EventEmitter();
 };
 
 MessageConnectorMock.prototype.reset = function() {
@@ -12,11 +15,16 @@ MessageConnectorMock.prototype.reset = function() {
 
 MessageConnectorMock.prototype.subscribe = function( topic, callback ) {
 	this.lastSubscribedTopic = topic;
+	this._eventEmitter.on( topic, callback );
 };
 
 MessageConnectorMock.prototype.publish = function( topic, message ) {
     this.lastPublishedTopic = topic;
     this.lastPublishedMessage = message;
+};
+
+MessageConnectorMock.prototype.simulateIncomingMessage = function( msg ) {
+    this._eventEmitter.emit( msg.topic, msg );
 };
 
 module.exports = MessageConnectorMock;
--- test/rpc/remote-rpc-provider-registrySpec.js
+++ test/rpc/remote-rpc-provider-registrySpec.js
@@ -1,5 +1,6 @@
 var RemoteRpcProviderRegistry = require( '../../src/rpc/remote-rpc-provider-registry' ),
 	MessageConnectorMock = require( '../mocks/message-connector-mock' ),
+	C = require( '../../src/constants/constants' ),
 	options = {
 		messageConnector: new MessageConnectorMock(),
 		rpcProviderQueryTimeout: 10,
@@ -14,7 +15,140 @@ describe( 'keeps track of which remote deepstream instance can provide which rpc
 	it( 'creates the registry', function(){
 		expect( options.messageConnector.lastSubscribedTopic ).toBe( null );
 		registry = new RemoteRpcProviderRegistry( options );
+		expect( options.messageConnector.lastSubscribedTopic ).toBe( C.TOPIC.RPC );
 	});
 	
+	it( 'queries other deepstream instances for remote providers', function(){
+		var rpcAProviderCallback = jasmine.createSpy( 'addTwoProviderCallback' );
+		
+		expect( options.messageConnector.lastPublishedMessage ).toBe( null );
+		
+		registry.getProviderTopic( 'rpcA', rpcAProviderCallback );
+		
+		expect( options.messageConnector.lastPublishedMessage ).toEqual({
+			topic: C.TOPIC.RPC,
+			action: C.ACTIONS.QUERY,
+			data: [ 'rpcA' ]
+		});
+		
+		expect( rpcAProviderCallback ).not.toHaveBeenCalled();
+		
+		options.messageConnector.simulateIncomingMessage({
+			topic: C.TOPIC.RPC,
+			action: C.ACTIONS.PROVIDER_UPDATE,
+			data: [{
+				numberOfProviders: 2,
+				privateTopic: 'privateTopicA',
+				rpcName: 'rpcA'
+			}]
+		});
+		
+		expect( rpcAProviderCallback.calls.length ).toBe( 1 );
+		expect( rpcAProviderCallback ).toHaveBeenCalledWith( null, 'privateTopicA' );
+		
+		// make sure it doesn't invoke callbacks multiple times for subsequent provider updates
+		options.messageConnector.simulateIncomingMessage({
+			topic: C.TOPIC.RPC,
+			action: C.ACTIONS.PROVIDER_UPDATE,
+			data: [{
+				numberOfProviders: 2,
+				privateTopic: 'privateTopicA',
+				rpcName: 'rpcA'
+			}]
+		});
+		
+		expect( rpcAProviderCallback.calls.length ).toBe( 1 );
+	});
 	
+	it( 'handles unsolicited provider updates', function(){
+		options.messageConnector.simulateIncomingMessage({
+			topic: C.TOPIC.RPC,
+			action: C.ACTIONS.PROVIDER_UPDATE,
+			data: [{
+				numberOfProviders: 2,
+				privateTopic: 'privateTopicB',
+				rpcName: 'rpcB'
+			}]
+		});
+		
+		registry.getProviderTopic( 'rpcB', function( error, topic ){
+			expect( topic ).toBe( 'privateTopicB' );
+		});
+		
+		expect( options.messageConnector.lastPublishedMessage.data[ 0 ] ).toBe( 'rpcA' );
+	});
+	
+	it( 'handles provider updates with multiple entries', function(){
+		options.messageConnector.simulateIncomingMessage({
+			topic: C.TOPIC.RPC,
+			action: C.ACTIONS.PROVIDER_UPDATE,
+			data: [{
+				numberOfProviders: 2,
+				privateTopic: 'privateTopicC',
+				rpcName: 'rpcC'
+			},{
+				numberOfProviders: 2,
+				privateTopic: 'privateTopicC',
+				rpcName: 'rpcD'
+			}]
+		});
+		
+		registry.getProviderTopic( 'rpcC', function( error, topic ){
+			expect( topic ).toBe( 'privateTopicC' );
+		});
+		
+		registry.getProviderTopic( 'rpcD', function( error, topic ){
+			expect( topic ).toBe( 'privateTopicC' );
+		});
+		
+		expect( options.messageConnector.lastPublishedMessage.data[ 0 ] ).toBe( 'rpcA' );
+	});
+	
+	it( 'returns different providers if multiple providers are available for the same rpc', function(){
+		options.messageConnector.simulateIncomingMessage({
+			topic: C.TOPIC.RPC,
+			action: C.ACTIONS.PROVIDER_UPDATE,
+			data: [{
+				numberOfProviders: 2,
+				privateTopic: 'privateTopicC',
+				rpcName: 'rpcA'
+			}]
+		});
+		
+		var hadTopicC = false,
+			hadTopicA = false;
+			
+		for( var i = 0; i < 200; i++ ) {
+			registry.getProviderTopic( 'rpcA', function( error, topic ){
+				if( topic === 'privateTopicA' ) {
+					hadTopicA = true;
+				}
+				
+				if( topic === 'privateTopicA' ) {
+					hadTopicC = true;
+				}
+			});
+		}
+		
+		expect( hadTopicA ).toBe( true );
+		expect( hadTopicC ).toBe( true );
+	});
+	
+	it( 'times out if query doesn\'t yield results in time', function( done ){
+		var providerCallback = jasmine.createSpy( 'providerCallback' );
+		
+		expect( options.messageConnector.lastPublishedMessage.data[ 0 ] ).toBe( 'rpcA' );
+		
+		registry.getProviderTopic( 'rpcE', providerCallback );
+		
+		expect( options.messageConnector.lastPublishedMessage.data[ 0 ] ).toBe( 'rpcE' );
+		
+		expect( providerCallback ).not.toHaveBeenCalled();
+		
+		setTimeout(function(){
+			expect( providerCallback.calls.length ).toBe( 1 );
+			expect( providerCallback ).toHaveBeenCalledWith( C.EVENT.NO_RPC_PROVIDER );
+			done();
+		}, 30 );
+	});
 });
