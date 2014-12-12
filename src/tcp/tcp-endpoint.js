ae32b92ba10fb9bfe619b3d32ff7e4ecff67fc77
hoxton-one <info@golden-layout.com>
2014-12-12 11:36:01 +0000

    Update tcp-endpoint.js

--- src/tcp/tcp-endpoint.js
+++ src/tcp/tcp-endpoint.js
@@ -4,7 +4,7 @@ var net = require( 'net' ),
 	TcpSocket = require( './tcp-socket' );
 
 /**
- * A faster, more low level alternative to engineIo when communicating
+ * A faster, more low level alternative to engine.io when communicating
  * with backend systems
  *
  * @emits connection <TcpSocket>
@@ -19,12 +19,39 @@ var TcpEndpoint = function( options ) {
 	this._options = options;
 	this._server = net.createServer( {allowHalfOpen: false}, this._onIncomingConnection.bind( this ) );
 	this._server.listen( this._options.host, this._options.port );
+	this._server.on( 'error', this._onError.bind( this ) );
 };
 
 utils.inherits( TcpEndpoint, event.EventEmitter );
 
+/**
+ * Callback for incoming connections. Creates an
+ * instance of TcpSocket that wraps the native net.Socket
+ * and emits a connection event.
+ *
+ * @param   {net.Socket} socket The original node socket instance
+ *
+ * @emits {TcpSocket} connection
+ *
+ * @private
+ * @returns {void}
+ */
 TcpEndpoint.prototype._onIncomingConnection = function( socket ) {
 	this.emit( 'connection', new TcpSocket( this._options, socket ) );
+};
+
+/**
+ * Callbacks for errors emitted by the server. Errors emitted by the
+ * sockets are directly handled by the TcpSocket class
+ *
+ * @param   {String} error
+ * @emits	{String} error
+ * 
+ * @private
+ * @returns {void}
+ */
+TcpEndpoint.prototype._onError = function( error ) {
+	this.emit( 'error', error.toString() );
 };
 
 module.exports = TcpEndpoint;
