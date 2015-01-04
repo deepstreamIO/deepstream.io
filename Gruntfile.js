78d09f1427e69497caf788d061cab1279cba1d41
Wolfram Hempel <hempel.wolfram@gmail.com>
2015-01-04 15:07:39 +0000

    Added grunt release task

---
+++ Gruntfile.js
@@ -0,0 +1,16 @@
+module.exports = function(grunt) {
+	grunt.initConfig({
+		pkg: grunt.file.readJSON( 'package.json' ),
+		release: {
+			options: {
+				github: { 
+					repo: 'hoxton-one/deepstream.io',
+					usernameVar: 'GITHUB_USERNAME',
+					passwordVar: 'GITHUB_PASSWORD'
+				}
+			}
+		}
+	});
+
+	grunt.loadNpmTasks( 'grunt-release' );
+};
