5032a921309fcebadfce2e4587134e0a776c2d92
Wolfram Hempel <hempel.wolfram@gmail.com>
2015-07-19 09:20:16 +0200

    Comments and tests for data-transformation class

--- src/message/data-transforms.js
+++ src/message/data-transforms.js
@@ -1,17 +1,21 @@
 var C = require( '../constants/constants' );
 
 /**
- * [DataTransforms description]
+ * This class converts the data-transformation settings the user specified as
+ * part of deepstream's options into an object that makes them easily
+ * accesible and executable.
  *
- * @param {[type]} settings An array of maps in the format
+ * @param {Array} settings An array of maps in the format
  *
  * [{
- * 	topic: <String>,
- * 	action: <String>,
- * 	transform: <Function>
+ * 	   topic: <String>,
+ * 	   action: <String>,
+ * 	   transform: <Function>
  * },
  * {...}
  * ]
+ *
+ * @constructor
  */
 var DataTransforms = function( settings ) {
 	this._transforms = {};
@@ -28,17 +32,52 @@ var DataTransforms = function( settings ) {
 	this._transforms[ C.TOPIC.RECORD ][ C.ACTIONS.UPDATE ] = null;
 	this._transforms[ C.TOPIC.RECORD ][ C.ACTIONS.PATCH ] = null;
 
+	if( !(settings instanceof Array ) ) {
+		throw new Error( 'option dataTransforms must be an array or null' );
+	}
+
 	settings.forEach( this._setTransform.bind( this ) );
 };
 
+/**
+ * Returns true if a transformation function has been registered for a
+ * topic/action combination
+ *
+ * @param   {String}  topic  One of C.TOPIC
+ * @param   {String}  action One of C.ACTION
+ *
+ * @public
+ * @returns {Boolean}
+ */
 DataTransforms.prototype.has = function( topic, action ) {
-	return this._transforms[ topic ] && this._transforms[ topic ][ action ];
+	return !!( this._transforms[ topic ] && this._transforms[ topic ][ action ] );
 };
 
-DataTransforms.prototype.apply = function( topic, action, data, username ) {
-	return this._transforms[ topic ][ action ]( data, username );
+/**
+ * Invokes the data-transform function. This will error when the transform
+ * function is not specified, so test before invocation with DataTransforms.has
+ *
+ * @param   {String}  topic  One of C.TOPIC
+ * @param   {String}  action One of C.ACTION
+ * @param   {Object}  data   The data that will be manipulated
+ * @param   {Object}  metaData Additional data
+ *
+ * @public
+ * @returns {Object} the manipulated data
+ */
+DataTransforms.prototype.apply = function( topic, action, data, metaData ) {
+	return this._transforms[ topic ][ action ]( data, metaData );
 };
 
+/**
+ * Called for every entry in the settings array. Validates the provided setting
+ * and stores it in the settings cache if valid
+ *
+ * @param {Object} setting An entry from the settings array that was provided to the constructor
+ *
+ * @private
+ * @returns {void}
+ */
 DataTransforms.prototype._setTransform = function( setting ) {
 	if( typeof setting !== 'object' ) {
 		throw new Error( 'transformation is not a map' );
