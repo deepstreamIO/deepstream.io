d2f70349c139cf6d8788ba034b8cdb63001804c5
Wolfram Hempel <hempel.wolfram@gmail.com>
2015-08-17 17:47:50 +0200

    Fixed minor test-fault

--- test/deepstreamioSpec.js
+++ test/deepstreamioSpec.js
@@ -3,7 +3,6 @@ var Deepstream = require( '../src/deepstream.io' );
 describe( 'the main server class', function(){
 	it( 'exposes the message parser\'s convertTyped method', function(){
 		var server = new Deepstream();
-
 		expect( server.convertTyped( 'N42' ) ).toBe( 42 );
 	});
 
