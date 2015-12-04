ec159deefca711333cf8eb4535f6651f39112747
Yasser Fadl <yasser.fadl@gmail.com>
2015-12-04 23:22:36 +0100

    Fixes #44 Server accepts clients after close event

--- test/mocks/engine-io-mock.js
+++ test/mocks/engine-io-mock.js
@@ -1,12 +1,15 @@
 var SocketMock = require( './socket-mock' );
 
-var EngineIoMock = function(){};
+var EngineIoMock = function(){
+	this.clients = [];
+};
 
 require("util").inherits( EngineIoMock, require("events").EventEmitter );
 
 EngineIoMock.prototype.simulateConnection = function() {
 	var socketMock = new SocketMock();
 	this.emit( 'connection', socketMock );
+	this.clients.push( socketMock );
 	return socketMock;
 };
 
@@ -16,6 +19,12 @@ EngineIoMock.prototype.listen = function(){
 
 EngineIoMock.prototype.attach = function( server ){
 	return this;
+};
+
+EngineIoMock.prototype.close = function(){
+	for( var i=0; i<this.clients.length; i++){
+		this.clients[i].close();
+	}
 };
 
 module.exports = new EngineIoMock();
