39decd86c759cabf974768ed9da2bb65d93757e3
Wolfram Hempel <hempel.wolfram@gmail.com>
2016-05-19 08:21:08 +0200

    moved copying of array out of for-loop and used slice instead

--- src/record/record-handler.js
+++ src/record/record-handler.js
@@ -375,7 +375,8 @@ RecordHandler.prototype._broadcastTransformedUpdate = function( transformUpdate,
 			recordName: name,
 			version: parseInt( message.data[ 1 ], 10 )
 		},
-		originalData = JSON.stringify( message.data ),
+		unparsedData = message.data[ transformUpdate ? 2 : 3 ],
+		messageData = message.data.slice( 0 ),
 		data,
 		i;
 
@@ -391,18 +392,17 @@ RecordHandler.prototype._broadcastTransformedUpdate = function( transformUpdate,
 
 		if( transformUpdate ) {
 			// UPDATE
-			data = JSON.parse( message.data[ 2 ] );
+			data = JSON.parse( unparsedData );
 			data = this._options.dataTransforms.apply( message.topic, message.action, data, metaData );
-			message.data[ 2 ] = JSON.stringify( data );
+			messageData[ 2 ] = JSON.stringify( data );
 		} else {
 			// PATCH
-			data = messageParser.convertTyped( message.data[ 3 ] );
+			data = messageParser.convertTyped( unparsedData );
 			data = this._options.dataTransforms.apply( message.topic, message.action, data, metaData );
-			message.data[ 3 ] = messageBuilder.typed( data );
+			messageData[ 3 ] = messageBuilder.typed( data );
 		}
 
-		receiver[ i ].sendMessage( message.topic, message.action, message.data );
-		message.data = JSON.parse( originalData );
+		receiver[ i ].sendMessage( message.topic, message.action, messageData );
 	}
 };
 
