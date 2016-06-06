3f178b3e0d8f3cc568bc50532ee5a9c49429e2c7
Yasser Fadl <yasser.fadl@gmail.com>
2016-06-06 11:55:08 +0200

    Downloading pre-built node from S3

---
+++ scripts/prepackage.sh
@@ -0,0 +1,10 @@
+OS=$( node scripts/details.js OS )
+
+if [ $OS = 'win32' ]; then
+
+	if [ ! -d "nexe_node" ]; then
+		curl -o nexe_node.7z https://s3.amazonaws.com/ds-server-artifacts/nexe_node_windows_ultra_4.4.5.7z
+		7z e nexe_node.7z -onexe_node
+	fi
+
+fi
