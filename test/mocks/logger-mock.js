8a7a8585d49ef72eef4834bff7aba2b35d276706
Yasser Fadl <yasser.fadl@gmail.com>
2016-06-08 12:54:52 +0200

    Removing log

--- test/mocks/logger-mock.js
+++ test/mocks/logger-mock.js
@@ -9,9 +9,6 @@ LoggerMock.prototype.log = function( level, event, message ) {
 		this.lastLogLevel = level;
 		this.lastLogEvent = event;
 		this.lastLogMessage = message;
-
-		if( this.lastLogLevel > 2 )
-			console.log( message )
 };
 
 LoggerMock.prototype.setLogLevel = function( level ) {
