6f189e3fe5bc1436b39740ef0c3a469acc9d1d3d
Anton Wilhelm <timaschew@gmail.com>
2016-06-14 12:29:39 +0200

    handle unsupported detached mode for windows

--- test/bin/cliSpec.js
+++ test/bin/cliSpec.js
@@ -2,11 +2,13 @@
 
 'use strict';
 
+const os = require( 'os' );
 const fs = require( 'fs' );
 const path = require( 'path' );
 const child_process = require( 'child_process' );
 const pidHelper = require( '../../bin/pid-helper' );
 
+const IS_WIN = os.platform() === 'win32';
 const cwd = path.resolve( '.' );
 const execOptions =  {
 	cwd: cwd,
@@ -31,6 +33,9 @@ describe( 'Command Line Interface', function() {
 	} );
 
 	it( 'start in background, status and stop', function() {
+		if ( IS_WIN ) {
+			return;
+		}
 		child_process.exec( 'node bin/deepstream start --detach', execOptions );
 		let output = '';
 		while ( output.indexOf( 'process running with PID' ) === -1 ) {
@@ -42,7 +47,20 @@ describe( 'Command Line Interface', function() {
 		expect( stopResult.stderr === '' ).toBe( true );
 	} );
 
+	it( 'start in background in windows not supported', function() {
+		if ( !IS_WIN ) {
+			return;
+		}
+		const result = child_process.spawnSync( 'node', ['bin/deepstream', 'start', '--detach'], execOptions );
+		expect( result.status ).toEqual( 1 );
+		expect( result.stderr ).toContain( 'windows' );
+		expect( result.stdout === '' ).toBe( true );
+	} );
+
 	it( 'fail during start of second server', function() {
+		if ( IS_WIN ) {
+			return;
+		}
 		child_process.exec( 'node bin/deepstream start', execOptions );
 		let output = '';
 		while ( output.indexOf( 'process running with PID' ) === -1 ) {
@@ -56,6 +74,9 @@ describe( 'Command Line Interface', function() {
 	} );
 
 	it( 'fail during start of second server, even if pid file not found', function() {
+		if ( IS_WIN ) {
+			return;
+		}
 		child_process.exec( 'node bin/deepstream start', execOptions );
 		let output = '';
 		while ( output.indexOf( 'process running with PID' ) === -1 ) {
