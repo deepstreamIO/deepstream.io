2a11cad537489000202353e701c068e81dd0c5a3
Anton Wilhelm <timaschew@gmail.com>
2016-06-20 15:47:11 +0200

    fix nexe, pin negotiator to 0.6.1

---
+++ scripts/shrinkwrap.js
@@ -0,0 +1,12 @@
+// fix for https://github.com/jaredallard/nexe/issues/219#issuecomment-221687043
+
+const fs = require( 'fs' );
+const path = require( 'path' );
+const shrinkwrapPath = '../npm-shrinkwrap.json';
+const shrinkwrap = require( shrinkwrapPath );
+
+const negotiator = shrinkwrap.dependencies['engine.io'].dependencies.accepts.dependencies.negotiator;
+negotiator.version = '0.6.1';
+negotiator.from = 'negotiator@0.6.1';
+negotiator.resolved = 'https://registry.npmjs.org/negotiator/-/negotiator-0.6.1.tgz';
+fs.writeFileSync( path.join(  __dirname, shrinkwrapPath ), JSON.stringify( shrinkwrap, null, 2 ) + '\n', 'utf8' );
