041ab1aa2aab37ff58c7462c385cdffe3ec4cb81
Alex <alexbruceharley@gmail.com>
2016-09-07 00:04:36 +1200

    fixing incorrect cluster support

--- src/presence/presence-handler.js
+++ src/presence/presence-handler.js
@@ -14,8 +14,27 @@ module.exports = class PresenceHandler {
 		this._connectionEndpoint = options.connectionEndpoint;
 		this._presenceRegistry = new SubscriptionRegistry( options, C.TOPIC.PRESENCE );
 		this._connectedClients = new DistributedStateRegistry( C.TOPIC.PRESENCE, options );
+		this._connectedClients.on( 'add', this._onClientAdded.bind( this ) );
+		this._connectedClients.on( 'remove', this._onClientRemoved.bind( this ) );
 	}
 
+	/**
+	 * The main entry point to the presence handler class.
+	 * Called on any of the following actions:
+	 *
+	 * 1) C.ACTIONS.PRESENCE_JOIN
+	 * 2) C.ACTIONS.PRESENCE_LEAVE
+	 * 3) C.ACTIONS.SUBSCRIBE
+	 *		a) C.ACTIONS.PRESENCE_JOIN
+	 *		b) C.ACTIONS.PRESENCE_LEAVE
+	 * 4) C.ACTIONS.QUERY
+	 *
+	 * @param   {SocketWrapper} socketWrapper the socket that send the request
+	 * @param   {Object} message parsed and validated message
+	 *
+	 * @public
+	 * @returns {void}
+	 */
 	handle( socketWrapper, message ) {
 		if( !socketWrapper.user ) {
 			return;
@@ -23,11 +42,9 @@ module.exports = class PresenceHandler {
 
 		if ( message.action === C.ACTIONS.PRESENCE_JOIN ) {
 			this._connectedClients.add( socketWrapper.user );
-			this._onClientAdded( socketWrapper );
 		} 
 		else if ( message.action === C.ACTIONS.PRESENCE_LEAVE ) {
 			this._connectedClients.remove( socketWrapper.user );
-			this._onClientRemoved( socketWrapper );
 		}
 		else if( message.action === C.ACTIONS.SUBSCRIBE ) {
 			if( message.data[ 0 ] === C.ACTIONS.PRESENCE_JOIN ) {
@@ -49,13 +66,31 @@ module.exports = class PresenceHandler {
 		}
 	}
 
-	_onClientAdded( socketWrapper ) {
-		var addMsg = messageBuilder.getMsg( C.TOPIC.PRESENCE, C.ACTIONS.PRESENCE_JOIN, [ socketWrapper.user ] );
-		this._presenceRegistry.sendToSubscribers( C.ACTIONS.PRESENCE_JOIN, addMsg, socketWrapper );
+	/**
+	 * Alerts all clients who are subscribed to
+	 * PRESENCE_JOIN that a new client has been added.
+	 * 
+	 * @param   {String} username the username of the client that joined
+	 *
+	 * @public
+	 * @returns {void}
+	 */
+	_onClientAdded( username ) {
+		var addMsg = messageBuilder.getMsg( C.TOPIC.PRESENCE, C.ACTIONS.PRESENCE_JOIN, [ username ] );
+		this._presenceRegistry.sendToSubscribers( C.ACTIONS.PRESENCE_JOIN, addMsg );
 	}
 
-	_onClientRemoved( socketWrapper ) {
-		var rmMsg = messageBuilder.getMsg( C.TOPIC.PRESENCE, C.ACTIONS.PRESENCE_LEAVE, [ socketWrapper.user ] );
-		this._presenceRegistry.sendToSubscribers( C.ACTIONS.PRESENCE_LEAVE, rmMsg, socketWrapper );
+	/**
+	 * Alerts all clients who are subscribed to
+	 * PRESENCE_LEAVE that the client has left.
+	 * 
+	 * @param   {String} username the username of the client that left
+	 *
+	 * @public
+	 * @returns {void}
+	 */
+	_onClientRemoved( username, socketWrapper ) {
+		var rmMsg = messageBuilder.getMsg( C.TOPIC.PRESENCE, C.ACTIONS.PRESENCE_LEAVE, [ username ] );
+		this._presenceRegistry.sendToSubscribers( C.ACTIONS.PRESENCE_LEAVE, rmMsg );
 	}
 }
