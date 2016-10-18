e7cad1ad761239e6372fb838126d3646898349f5
Robert Nagy <ronag89@gmail.com>
2016-10-18 10:05:55 +0200

    record: docs are not updated. Removed them.

--- src/record/record-handler.js
+++ src/record/record-handler.js
@@ -7,11 +7,6 @@ var C = require( '../constants/constants' ),
 	EventEmitter = require( 'events' ).EventEmitter,
 	LRU = require('lru-cache');
 
-/**
- * The entry point for record related operations
- *
- * @param {Object} options deepstream options
- */
 var RecordHandler = function( options ) {
 	this._options = options;
 	this._subscriptionRegistry = new SubscriptionRegistry( options, C.TOPIC.RECORD );
@@ -30,55 +25,27 @@ var RecordHandler = function( options ) {
 	this._cache = new LRU({ max: this._options.cacheSize || 1e5 });
 };
 
-/**
- * Handles incoming record requests.
- *
- * @param   {SocketWrapper} socketWrapper the sender
- * @param   {Object} message parsed and validated deepstream message
- *
- * @public
- * @returns {void}
- */
 RecordHandler.prototype.handle = function( socketWrapper, message ) {
-
-	/*
-	 * All messages have to provide at least the name of the record they relate to
-	 * or a pattern in case of listen
-	 */
 	if( !message.data || message.data.length < 1 ) {
 		socketWrapper.sendError( C.TOPIC.RECORD, C.EVENT.INVALID_MESSAGE_DATA, message.raw );
 		return;
 	}
 
-	/*
-	 * Return the record's contents and subscribes for future updates.
-	 */
 	if( message.action === C.ACTIONS.READ ) {
 		this._read( socketWrapper, message );
 		return;
 	}
 
-	/*
-	 * Handle complete (UPDATE)
-	 */
 	if( message.action === C.ACTIONS.UPDATE ) {
 		this._update( socketWrapper, message );
 		return;
 	}
 
-	/*
-	 * Unsubscribes (discards) a record that was previously subscribed to
-	 * using read()
-	 */
 	if( message.action === C.ACTIONS.UNSUBSCRIBE ) {
 		this._unsubscribe( socketWrapper, message );
 		return;
 	}
 
-	/*
-	 * Listen to requests for a particular record or records
-	 * whose names match a pattern
-	 */
 	if( message.action === C.ACTIONS.LISTEN ||
 		message.action === C.ACTIONS.UNLISTEN ||
 		message.action === C.ACTIONS.LISTEN_ACCEPT ||
@@ -88,9 +55,6 @@ RecordHandler.prototype.handle = function( socketWrapper, message ) {
 		return;
 	}
 
-	/*
-	 * Default for invalid messages
-	 */
 	this._logger.log( C.LOG_LEVEL.WARN, C.EVENT.UNKNOWN_ACTION, message.action );
 
 	if( socketWrapper.sendError ) {
@@ -98,16 +62,6 @@ RecordHandler.prototype.handle = function( socketWrapper, message ) {
 	}
 };
 
-/**
- * Tries to retrieve the record and creates it if it doesn't exist. Please
- * note that create also triggers a read once done
- *
- * @param   {SocketWrapper} socketWrapper the socket that send the request
- * @param   {Object} message parsed and validated message
- *
- * @private
- * @returns {void}
- */
 RecordHandler.prototype._read = function( socketWrapper, message ) {
 	const recordName = message.data[ 0 ];
 	this.getRecord( recordName )
@@ -122,16 +76,6 @@ RecordHandler.prototype._read = function( socketWrapper, message ) {
 		.catch( error => socketWrapper.sendError( C.TOPIC.RECORD, error.event, [ recordName, error.message ] ) );
 };
 
- /**
- * Applies both full and partial updates. Creates a new record transition that will live as long as updates
- * are in flight and new updates come in
- *
- * @param   {SocketWrapper} socketWrapper the socket that send the request
- * @param   {Object} message parsed and validated message
- *
- * @private
- * @returns {void}
- */
 RecordHandler.prototype._update = function( socketWrapper, message ) {
 	const recordName = message.data[ 0 ];
 
@@ -188,16 +132,6 @@ RecordHandler.prototype._unsubscribe = function( socketWrapper, message ) {
 	this._subscriptionRegistry.unsubscribe( recordName, socketWrapper );
 }
 
-/**
- * Sends the records data current data once done
- *
- * @param {String} recordName
- * @param {Object} record
- * @param {SocketWrapper} socketWrapper the socket that send the request
- *
- * @private
- * @returns {void}
- */
 RecordHandler.prototype._sendRecord = function( recordName, record, socketWrapper ) {
 	let data = record._d;
 
@@ -213,18 +147,6 @@ RecordHandler.prototype._sendRecord = function( recordName, record, socketWrappe
 	socketWrapper.sendMessage( C.TOPIC.RECORD, C.ACTIONS.READ, [ recordName, record._v, data ] );
 };
 
-/**
- * Called by _update if registered transform functions are detected. Disassembles
- * the message and invokes the transform function prior to sending it to every individual receiver
- * so that receiver specific transforms can be applied.
- *
- * @param   {String} recordName       the record name
- * @param   {Object} message          a parsed deepstream message object
- * @param   {SocketWrapper|String} originalSender  the original sender of the update or a string pointing at the messageBus
- *
- * @private
- * @returns {void}
- */
 RecordHandler.prototype._broadcastTransformedUpdate = function( recordName, message, originalSender ) {
 	const receivers = this._subscriptionRegistry.getLocalSubscribers( recordName ) || [];
 
@@ -246,18 +168,6 @@ RecordHandler.prototype._broadcastTransformedUpdate = function( recordName, mess
 	}
 };
 
-/**
- * Executes or schedules a callback function once all transitions are complete
- *
- * This is called from the PermissionHandler destroy method, which
- * could occur in cases where 'runWhenRecordStable' is never called,
- * such as when no cross referencing or data loading is used.
- *
- * @param   {String}   recordName the name of the record
- *
- * @private
- * @returns {void}
- */
 RecordHandler.prototype.removeRecordRequest = function( recordName ) {
 	if( !this._recordRequestsInProgress[ recordName ] ) {
 		return;
@@ -271,18 +181,6 @@ RecordHandler.prototype.removeRecordRequest = function( recordName ) {
 	this._recordRequestsInProgress[ recordName ].splice( 0, 1 )[ 0 ]();
 };
 
-/**
- * Executes or schedules a callback function once all record requests are removed.
- * This is critical to block reads until writes have occured for a record, which is
- * only from permissions when a rule is required to be run and the cache has not
- * verified it has the latest version
- *
- * @param   {String}   recordName the name of the record
- * @param   {Function} callback   function to be executed once all writes to this record are complete
- *
- * @public
- * @returns {void}
- */
 RecordHandler.prototype.runWhenRecordStable = function( recordName, callback ) {
 	if( !this._recordRequestsInProgress[ recordName ] ) {
 		this._recordRequestsInProgress[ recordName ] = [];
@@ -292,17 +190,6 @@ RecordHandler.prototype.runWhenRecordStable = function( recordName, callback ) {
 	}
 };
 
-/**
- * A secondary permissioning step that is performed once we know if the record exists (READ).
- *
- * @param   {String} action          Action to check permissions on
- * @param   {String} recordName      The name of the record
- * @param   {SocketWrapper} socketWrapper the socket that send the request
- * @param   {Function} successCallback A callback that will only be invoked if the operation was successful
- *
- * @private
- * @returns {void}
- */
 RecordHandler.prototype._permissionAction = function( action, recordName, socketWrapper ) {
 	return new Promise( ( resolve, reject ) => {
 		const message = {
@@ -358,14 +245,6 @@ RecordHandler.prototype._getRecordFromStorage = function ( recordName ) {
 	} ) );
 }
 
-/**
- * Callback for changes from storage
- *
- * @param   {String} error
- *
- * @private
- * @returns {void}
- */
 RecordHandler.prototype._onStorageChange = function( recordName, version ) {
 	const prevRecord = this._cache.get( recordName );
 
