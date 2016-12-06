1e6f67202a733d45108088aeda73773b78cd7ca8
Alex Harley <alexbruceharley@gmail.com>
2016-12-06 17:31:38 +0100

    adding setup handshake data and tests

--- test/message/socket-wrapperSpec.js
+++ test/message/socket-wrapperSpec.js
@@ -2,15 +2,25 @@ var SocketMock = require( '../mocks/socket-mock' );
 var SocketWrapper = require( '../../src/message/socket-wrapper' );
 
 describe( 'socket-wrapper creates a unified interface for sockets', function(){
+	var socket = new SocketMock(),
+		socketWrapper;
+	socket._socket.remoteAddress = 'some-address';
+	socket.upgradeReq = {
+		headers: {
+			referer: 'some-referer'
+		}
+	};
+
 	it( 'creates a SocketWrapper', function(){
-		var socket = new SocketMock();
-		socket._socket.remoteAddress = 'some-address';
-		socket.upgradeReq = {
-			headers: {
-				referer: 'some-referer'
-			}
-		};
-		var socketWrapper = new SocketWrapper( socket, {} );
+		socketWrapper = new SocketWrapper( socket, {} );
+		expect( socketWrapper.getHandshakeData() ).toEqual({
+			headers: { referer: 'some-referer' },
+			referer: 'some-referer',
+			remoteAddress: 'some-address'
+		});
+	});
+
+	it( 'handshake data is able to be queried for again', function() {
 		expect( socketWrapper.getHandshakeData() ).toEqual({
 			headers: { referer: 'some-referer' },
 			referer: 'some-referer',
