eb6f17078cef1e00edbdc094648b5567451ccd11
yasserf <yasser.fadl@gmail.com>
2016-12-11 23:29:04 +0100

    Turning some core objects into es6 classes as preperation for optimizations

--- src/message/message-distributor.js
+++ src/message/message-distributor.js
@@ -8,70 +8,73 @@ const C = require('../constants/constants')
  *
  * @param {Object} options deepstream options
  */
-const MessageDistributor = function (options) {
-  this._callbacks = {}
-  this._options = options
-}
-
-/**
- * Accepts a socketWrapper and a parsed message as input and distributes
- * it to its subscriber, based on the message's topic
- *
- * @param   {SocketWrapper} socketWrapper
- * @param   {Object} message parsed and permissioned message
- *
- * @public
- * @returns {void}
- */
-MessageDistributor.prototype.distribute = function (socketWrapper, message) {
-  if (this._callbacks[message.topic] === undefined) {
-    this._options.logger.log(C.LOG_LEVEL.WARN, C.EVENT.UNKNOWN_TOPIC, message.topic)
-    socketWrapper.sendError(C.TOPIC.ERROR, C.EVENT.UNKNOWN_TOPIC, message.topic)
-    return
+module.exports = class MessageDistributor {
+  constructor(options) {
+    this._callbacks = {}
+    this._options = options
   }
 
-  socketWrapper.emit(message.topic, message)
+  /**
+   * Accepts a socketWrapper and a parsed message as input and distributes
+   * it to its subscriber, based on the message's topic
+   *
+   * @param   {SocketWrapper} socketWrapper
+   * @param   {Object} message parsed and permissioned message
+   *
+   * @public
+   * @returns {void}
+   */
+  distribute(socketWrapper, message) {
+    if (this._callbacks[message.topic] === undefined) {
+      this._options.logger.log(C.LOG_LEVEL.WARN, C.EVENT.UNKNOWN_TOPIC, message.topic)
+      socketWrapper.sendError(C.TOPIC.ERROR, C.EVENT.UNKNOWN_TOPIC, message.topic)
+      return
+    }
 
-  if (message.isCompleted !== true) {
-    this._callbacks[message.topic](socketWrapper, message)
+    socketWrapper.emit(message.topic, message)
+
+    if (message.isCompleted !== true) {
+      this._callbacks[message.topic](socketWrapper, message)
+    }
+  }
+
+  /**
+   * Allows handlers (event, rpc, record) to register for topics. Subscribes them
+   * to both messages passed to the distribute method as well as messages received
+   * from the messageConnector
+   *
+   * @param   {String}   topic    One of C.TOPIC
+   * @param   {Function} callback The method that should be called for every message
+   *                              on the specified topic. Will be called with socketWrapper
+   *                              and message
+   *
+   * @public
+   * @returns {void}
+   */
+  registerForTopic(topic, callback) {
+    if (this._callbacks[topic] !== undefined) {
+      throw new Error(`Callback already registered for topic ${topic}`)
+    }
+
+    this._callbacks[topic] = callback
+    this._options.messageConnector.subscribe(
+      topic,
+      this._onMessageConnectorMessage.bind(this, callback)
+    )
+  }
+
+  /**
+   * Whenever a message from the messageConnector is received it is passed
+   * to the relevant handler, but with SOURCE_MESSAGE_CONNECTOR instead of
+   * a socketWrapper as sender
+   *
+   * @param   {Function} callback the handler callback
+   * @param   {Object}   message  the already parsed and validated message
+   *
+   * @private
+   * @returns {void}
+   */
+  _onMessageConnectorMessage(callback, message) {
+    callback(C.SOURCE_MESSAGE_CONNECTOR, message)
   }
 }
-
-/**
- * Allows handlers (event, rpc, record) to register for topics. Subscribes them
- * to both messages passed to the distribute method as well as messages received
- * from the messageConnector
- *
- * @param   {String}   topic    One of C.TOPIC
- * @param   {Function} callback The method that should be called for every message
- *                              on the specified topic. Will be called with socketWrapper
- *                              and message
- *
- * @public
- * @returns {void}
- */
-MessageDistributor.prototype.registerForTopic = function (topic, callback) {
-  if (this._callbacks[topic] !== undefined) {
-    throw new Error(`Callback already registered for topic ${topic}`)
-  }
-
-  this._callbacks[topic] = callback
-  this._options.messageConnector.subscribe(topic, this._onMessageConnectorMessage.bind(this, callback))
-}
-
-/**
- * Whenever a message from the messageConnector is received it is passed
- * to the relevant handler, but with SOURCE_MESSAGE_CONNECTOR instead of
- * a socketWrapper as sender
- *
- * @param   {Function} callback the handler callback
- * @param   {Object}   message  the already parsed and validated message
- *
- * @private
- * @returns {void}
- */
-MessageDistributor.prototype._onMessageConnectorMessage = function (callback, message) {
-  callback(C.SOURCE_MESSAGE_CONNECTOR, message)
-}
-
-module.exports = MessageDistributor
--- src/message/message-processor.js
+++ src/message/message-processor.js
@@ -12,104 +12,114 @@ const C = require('../constants/constants')
  *
  * @param {Object} options deepstream options
  */
-const MessageProcessor = function (options) {
-  this._options = options
-}
+module.exports = class MessageProcessor {
 
-/**
- * There will only ever be one consumer of forwarded messages. So rather than using
- * events - and their performance overhead - the messageProcessor exposes
- * this method that's expected to be overwritten.
- *
- * @param   {SocketWrapper} socketWrapper
- * @param   {Object} message the parsed message
- *
- * @overwrite
- *
- * @returns {void}
- */
-MessageProcessor.prototype.onAuthenticatedMessage = function (socketWrapper, message) {}
+  constructor(options) {
+    this._options = options
+  }
 
-/**
- * This method is the way the message processor accepts input. It receives arrays
- * of parsed messages, iterates through them and issues permission requests for
- * each individual message
- *
- * @todo The responses from the permissionHandler might arive in any arbitrary order - order them
- * @todo Handle permission handler timeouts
- *
- * @param   {SocketWrapper} socketWrapper
- * @param   {Object} message parsed message
- *
- * @returns {void}
- */
-MessageProcessor.prototype.process = function (socketWrapper, message) {
-  const parsedMessages = messageParser.parse(message)
-  let parsedMessage
+  /**
+   * There will only ever be one consumer of forwarded messages. So rather than using
+   * events - and their performance overhead - the messageProcessor exposes
+   * this method that's expected to be overwritten.
+   *
+   * @param   {SocketWrapper} socketWrapper
+   * @param   {Object} message the parsed message
+   *
+   * @overwrite
+   *
+   * @returns {void}
+   */
+  onAuthenticatedMessage(socketWrapper, message) {
+  }
 
-  const length = parsedMessages.length
-  for (let i = 0; i < length; i++) {
-    parsedMessage = parsedMessages[i]
+  /**
+   * This method is the way the message processor accepts input. It receives arrays
+   * of parsed messages, iterates through them and issues permission requests for
+   * each individual message
+   *
+   * @todo The responses from the permissionHandler might arive in any arbitrary order - order them
+   * @todo Handle permission handler timeouts
+   *
+   * @param   {SocketWrapper} socketWrapper
+   * @param   {Object} message parsed message
+   *
+   * @returns {void}
+   */
+  process(socketWrapper, message) {
+    const parsedMessages = messageParser.parse(message)
+    let parsedMessage
 
-    if (parsedMessage === null) {
-      this._options.logger.log(C.LOG_LEVEL.WARN, C.EVENT.MESSAGE_PARSE_ERROR, message)
-      socketWrapper.sendError(C.TOPIC.ERROR, C.EVENT.MESSAGE_PARSE_ERROR, message)
-      continue
+    const length = parsedMessages.length
+    for (let i = 0; i < length; i++) {
+      parsedMessage = parsedMessages[i]
+
+      if (parsedMessage === null) {
+        this._options.logger.log(C.LOG_LEVEL.WARN, C.EVENT.MESSAGE_PARSE_ERROR, message)
+        socketWrapper.sendError(C.TOPIC.ERROR, C.EVENT.MESSAGE_PARSE_ERROR, message)
+        continue
+      }
+
+      if (parsedMessage.topic === C.TOPIC.CONNECTION && parsedMessage.action === C.ACTIONS.PONG) {
+        continue
+      }
+
+      this._options.permissionHandler.canPerformAction(
+        socketWrapper.user,
+        parsedMessage,
+        this._onPermissionResponse.bind(this, socketWrapper, parsedMessage),
+        socketWrapper.authData
+      )
+    }
+  }
+
+  /**
+   * Processes the response that's returned by the permissionHandler.
+   *
+   * @param   {SocketWrapper}   socketWrapper
+   * @param   {Object} message  parsed message - might have been manipulated
+   *                              by the permissionHandler
+   * @param   {Error} error     error or null if no error. Denied permissions will be expressed
+   *                            by setting result to false
+   * @param   {Boolean} result    true if permissioned
+   *
+   * @returns {void}
+   */
+  _onPermissionResponse(socketWrapper, message, error, result) {
+    if (error !== null) {
+      this._options.logger.log(C.LOG_LEVEL.WARN, C.EVENT.MESSAGE_PERMISSION_ERROR, error.toString())
+      socketWrapper.sendError(
+        message.topic,
+        C.EVENT.MESSAGE_PERMISSION_ERROR,
+        this._getPermissionErrorData(message)
+      )
+      return
     }
 
-    if (parsedMessage.topic === C.TOPIC.CONNECTION && parsedMessage.action === C.ACTIONS.PONG) {
-      continue
+    if (result !== true) {
+      socketWrapper.sendError(
+        message.topic,
+        C.EVENT.MESSAGE_DENIED,
+        this._getPermissionErrorData(message)
+      )
+      return
     }
 
-    this._options.permissionHandler.canPerformAction(
-      socketWrapper.user,
-      parsedMessage,
-      this._onPermissionResponse.bind(this, socketWrapper, parsedMessage),
-      socketWrapper.authData
-    )
+    this.onAuthenticatedMessage(socketWrapper, message)
+  }
+
+  /**
+   * Create data in the correct format expected in a MESSAGE_DENIED or MESSAGE_PERMISSION_ERROR
+   *
+   * @param   {Object} message  parsed message - might have been manipulated
+   *                              by the permissionHandler
+   * @returns {Object}
+   */
+  _getPermissionErrorData(message) {
+    let data = [message.data[0], message.action]
+    if (message.data.length > 1) {
+      data = data.concat(message.data.slice(1))
+    }
+    return data
   }
 }
-
-/**
- * Processes the response that's returned by the permissionHandler.
- *
- * @param   {SocketWrapper}   socketWrapper
- * @param   {Object} message  parsed message - might have been manipulated
- *                              by the permissionHandler
- * @param   {Error} error     error or null if no error. Denied permissions will be expressed
- *                            by setting result to false
- * @param   {Boolean} result    true if permissioned
- *
- * @returns {void}
- */
-MessageProcessor.prototype._onPermissionResponse = function (socketWrapper, message, error, result) {
-  if (error !== null) {
-    this._options.logger.log(C.LOG_LEVEL.WARN, C.EVENT.MESSAGE_PERMISSION_ERROR, error.toString())
-    socketWrapper.sendError(message.topic, C.EVENT.MESSAGE_PERMISSION_ERROR, this._getPermissionErrorData(message))
-    return
-  }
-
-  if (result !== true) {
-    socketWrapper.sendError(message.topic, C.EVENT.MESSAGE_DENIED, this._getPermissionErrorData(message))
-    return
-  }
-
-  this.onAuthenticatedMessage(socketWrapper, message)
-}
-
-/**
- * Create data in the correct format expected in a MESSAGE_DENIED or MESSAGE_PERMISSION_ERROR
- *
- * @param   {Object} message  parsed message - might have been manipulated
- *                              by the permissionHandler
- * @returns {Object}
- */
-MessageProcessor.prototype._getPermissionErrorData = function (message) {
-  let data = [message.data[0], message.action]
-  if (message.data.length > 1) {
-    data = data.concat(message.data.slice(1))
-  }
-  return data
-}
-
-module.exports = MessageProcessor
