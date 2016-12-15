f0175c5775d406388806b30809b8bbb42211af2f
Alex Harley <alexbruceharley@gmail.com>
2016-12-15 12:24:40 +0100

    travis

--- test/deepstreamioSpec.js
+++ test/deepstreamioSpec.js
@@ -184,7 +184,7 @@ describe('handle server startup without config file', () => {
   const execOptions = {
     cwd,
     //stdio: ['ignore', 'ignore', 'pipe'],
-    env: { configDirs: ['/'] }
+    env: { configDirs: [path.join('..', '/', '..', '/', '..')] }
   }
 
   it('via CLI', (done) => {
