9389c037ac3dcc9e716fd7203dc6d1cd2f3dbe5f
Robert Nagy <ronag89@gmail.com>
2017-02-04 23:28:18 +0100

    cacheConnector TODO

--- src/default-plugins/local-cache.js
+++ src/default-plugins/local-cache.js
@@ -1,20 +1,17 @@
 const LocalCache = function () {
   this.isReady = true
-  this._data = {}
 }
 
 LocalCache.prototype.set = function (key, value, callback) {
-  this._data[key] = value
-  callback(null)
+  callback(null, key)
 }
 
 LocalCache.prototype.get = function (key, callback) {
-  callback(null, this._data[key] || null)
+  callback(null, key, null)
 }
 
 LocalCache.prototype.delete = function (key, callback) {
-  delete this._data[key]
-  callback(null)
+  callback(null, key)
 }
 
 module.exports = new LocalCache()
