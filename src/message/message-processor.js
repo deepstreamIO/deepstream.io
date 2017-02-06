0e6d5d1b885df85a6da068a595f12ff83d516374
Robert Nagy <ronag89@gmail.com>
2017-02-06 15:09:12 +0100

    ensure message ordering is maintained through permissions handler

--- src/message/message-processor.js
+++ src/message/message-processor.js
@@ -1,6 +1,68 @@
 const messageParser = require('./message-parser')
 const C = require('../constants/constants')
 
+const Queue = function (parent, socketWrapper) {
+  this.onAuthenticatedMessage = parent.onAuthenticatedMessage
+  this.permissionHandler = parent._options.permissionHandler
+  this.logger = parent._options.logger
+  this.socketWrapper = socketWrapper
+  this.messages = []
+  this.running = false
+
+  this._next = this._next.bind(this)
+}
+
+Queue.prototype.process = function (message) {
+  for (const parsedMessage of messageParser.parse(message)) {
+    if (parsedMessage === null) {
+      this._options.logger.log(C.LOG_LEVEL.WARN, C.EVENT.MESSAGE_PARSE_ERROR, message)
+      this.socketWrapper.sendError(C.TOPIC.ERROR, C.EVENT.MESSAGE_PARSE_ERROR, message)
+      continue
+    }
+
+    if (parsedMessage.topic === C.TOPIC.CONNECTION && parsedMessage.action === C.ACTIONS.PONG) {
+      continue
+    }
+
+    this.messages.push(parsedMessage)
+  }
+
+  this._next()
+}
+
+Queue.prototype._next = function (recursive) {
+  if (this.running || this.messages.length === 0) {
+    return
+  }
+
+  this.running = true
+  this.permissionHandler.canPerformAction(
+    this.socketWrapper.user,
+    this.messages[0],
+    (error, result) => {
+      this.running = false
+
+      const parsedMessage = this.messages.shift()
+
+      if (error !== null) {
+        this.logger.log(C.LOG_LEVEL.WARN, C.EVENT.MESSAGE_PERMISSION_ERROR, error.toString())
+        this.socketWrapper.sendError(parsedMessage.topic, C.EVENT.MESSAGE_PERMISSION_ERROR, getPermissionErrorData(parsedMessage))
+        return process.nextTick(this._next)
+      }
+
+      if (result !== true) {
+        this.socketWrapper.sendError(parsedMessage.topic, C.EVENT.MESSAGE_DENIED, getPermissionErrorData(parsedMessage))
+        return process.nextTick(this._next)
+      }
+
+      this.onAuthenticatedMessage(this.socketWrapper, parsedMessage)
+
+      return process.nextTick(this._next)
+    },
+    this.socketWrapper.authData
+  )
+}
+
 /**
  * The MessageProcessor consumes blocks of parsed messages emitted by the
  * ConnectionEndpoint, checks if they are permissioned and - if they
@@ -12,6 +74,7 @@ const C = require('../constants/constants')
  */
 const MessageProcessor = function (options) {
   this._options = options
+  this._queues = new Map()
 }
 
 /**
@@ -42,57 +105,13 @@ MessageProcessor.prototype.onAuthenticatedMessage = function (socketWrapper, mes
  * @returns {void}
  */
 MessageProcessor.prototype.process = function (socketWrapper, message) {
-  const parsedMessages = messageParser.parse(message)
-  let parsedMessage
-
-  const length = parsedMessages.length
-  for (let i = 0; i < length; i++) {
-    parsedMessage = parsedMessages[i]
-
-    if (parsedMessage === null) {
-      this._options.logger.log(C.LOG_LEVEL.WARN, C.EVENT.MESSAGE_PARSE_ERROR, message)
-      socketWrapper.sendError(C.TOPIC.ERROR, C.EVENT.MESSAGE_PARSE_ERROR, message)
-      continue
-    }
-
-    if (parsedMessage.topic === C.TOPIC.CONNECTION && parsedMessage.action === C.ACTIONS.PONG) {
-      continue
-    }
-
-    this._options.permissionHandler.canPerformAction(
-      socketWrapper.user,
-      parsedMessage,
-      this._onPermissionResponse.bind(this, socketWrapper, parsedMessage),
-      socketWrapper.authData
-    )
+  let queue = this._queues.get(socketWrapper)
+  if (!queue) {
+    queue = new Queue(this, socketWrapper)
+    this._queues.set(socketWrapper, queue)
+    socketWrapper.on('close', () => this._queues.delete(socketWrapper))
   }
-}
-
-/**
- * Processes the response that's returned by the permissionHandler.
- *
- * @param   {SocketWrapper}   socketWrapper
- * @param   {Object} message  parsed message - might have been manipulated
- *                              by the permissionHandler
- * @param   {Error} error     error or null if no error. Denied permissions will be expressed
- *                            by setting result to false
- * @param   {Boolean} result    true if permissioned
- *
- * @returns {void}
- */
-MessageProcessor.prototype._onPermissionResponse = function (socketWrapper, message, error, result) {
-  if (error !== null) {
-    this._options.logger.log(C.LOG_LEVEL.WARN, C.EVENT.MESSAGE_PERMISSION_ERROR, error.toString())
-    socketWrapper.sendError(message.topic, C.EVENT.MESSAGE_PERMISSION_ERROR, this._getPermissionErrorData(message))
-    return
-  }
-
-  if (result !== true) {
-    socketWrapper.sendError(message.topic, C.EVENT.MESSAGE_DENIED, this._getPermissionErrorData(message))
-    return
-  }
-
-  this.onAuthenticatedMessage(socketWrapper, message)
+  queue.process(message)
 }
 
 /**
@@ -102,8 +121,8 @@ MessageProcessor.prototype._onPermissionResponse = function (socketWrapper, mess
  *                              by the permissionHandler
  * @returns {Object}
  */
-MessageProcessor.prototype._getPermissionErrorData = function (message) {
-  let data = [message.data[0], message.action]
+function getPermissionErrorData (message) {
+  let data = [ message.data[0], message.action ]
   if (message.data.length > 1) {
     data = data.concat(message.data.slice(1))
   }
