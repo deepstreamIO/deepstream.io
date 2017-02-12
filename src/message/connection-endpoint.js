0a26fff8d5c323f63f255df3186ed28fb22501b7
yasserf <yasser.fadl@gmail.com>
2017-02-12 19:37:54 +0100

    Fixes #538 Non text based websocket frame

--- src/message/connection-endpoint.js
+++ src/message/connection-endpoint.js
@@ -178,6 +178,20 @@ module.exports = class ConnectionEndpoint extends events.EventEmitter {
    * @returns {void}
    */
   _processConnectionMessage (socketWrapper, connectionMessage) {
+    if (typeof connectionMessage !== 'string') {
+      this._options.logger.log(
+        C.LOG_LEVEL.WARN,
+        C.EVENT.INVALID_MESSAGE,
+        connectionMessage.toString()
+      )
+      socketWrapper.sendError(
+        C.TOPIC.CONNECTION,
+        C.EVENT.INVALID_MESSAGE,
+        'invalid connection message'
+      )
+      return
+    }
+
     const msg = messageParser.parse(connectionMessage)[0]
 
     if (msg === null || msg === undefined) {
@@ -213,6 +227,20 @@ module.exports = class ConnectionEndpoint extends events.EventEmitter {
    * @returns {void}
    */
   _authenticateConnection (socketWrapper, disconnectTimeout, authMsg) {
+    if (typeof authMsg !== 'string') {
+      this._options.logger.log(
+        C.LOG_LEVEL.WARN,
+        C.EVENT.INVALID_AUTH_MSG,
+        authMsg.toString()
+      )
+      socketWrapper.sendError(
+        C.TOPIC.AUTH,
+        C.EVENT.INVALID_AUTH_MSG,
+        'invalid authentication message'
+      )
+      return
+    }
+
     const msg = messageParser.parse(authMsg)[0]
     let authData
     let errorMsg
