78eb99547e6147e7c638d043ad2af4313aae8792
Robert Nagy <ronag89@gmail.com>
2017-02-27 14:07:15 +0100

    docs(recordi-handler): TODO

--- src/record/record-handler.js
+++ src/record/record-handler.js
@@ -115,6 +115,7 @@ module.exports = class RecordHandler {
           const message = `error while reading ${record[0]} from storage`
           this._sendError(socket, C.EVENT.RECORD_LOAD_ERROR, [ ...record, message ])
         } else {
+          // TODO: Check version
           this._broadcast(record)
         }
       })
