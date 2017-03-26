88ed68b09704e8844591989b9661fb5bd71c2551
Robert Nagy <ronag89@gmail.com>
2017-03-26 13:32:04 +0200

    Revert "perf(message-parser): Removed unnecessary checks."
    This reverts commit d9f1f66cdecca95353d9215c233a50f528e0edf1.

--- src/message/message-parser.js
+++ src/message/message-parser.js
@@ -3,6 +3,16 @@
 const C = require('../constants/constants')
 
 /**
+ * Turns the ACTION:SHORTCODE constants map
+ * around to facilitate shortcode lookup
+ *
+ * @private
+ *
+ * @returns {Object} actions
+*/
+const actions = new Set(Object.keys(C.ACTIONS).map(key => C.ACTIONS[key]))
+
+/**
  * Parses ASCII control character seperated
  * message strings into digestable maps
  *
@@ -32,7 +42,9 @@ module.exports = class MessageParser {
     const rawMessages = message.split(C.MESSAGE_SEPERATOR)
 
     for (let i = 0; i < rawMessages.length; i++) {
-      callback(this.parseMessage(rawMessages[i]), message)
+      if (rawMessages[i].length > 2) {
+        callback(this.parseMessage(rawMessages[i]), message)
+      }
     }
   }
 
@@ -48,7 +60,16 @@ module.exports = class MessageParser {
    */
   static parseMessage (message) {
     const parts = message.split(C.MESSAGE_PART_SEPERATOR)
-    return parts.length < 2 ? null : {
+
+    if (parts.length < 2) {
+      return null
+    }
+
+    if (!actions.has(parts[1])) {
+      return null
+    }
+
+    return {
       raw: message,
       topic: parts[0],
       action: parts[1],
