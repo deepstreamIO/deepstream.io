d9f1f66cdecca95353d9215c233a50f528e0edf1
Robert Nagy <ronag89@gmail.com>
2017-03-26 12:46:35 +0200

    perf(message-parser): Removed unnecessary checks.

--- src/message/message-parser.js
+++ src/message/message-parser.js
@@ -3,16 +3,6 @@
 const C = require('../constants/constants')
 
 /**
- * Turns the ACTION:SHORTCODE constants map
- * around to facilitate shortcode lookup
- *
- * @private
- *
- * @returns {Object} actions
-*/
-const actions = new Set(Object.keys(C.ACTIONS).map(key => C.ACTIONS[key]))
-
-/**
  * Parses ASCII control character seperated
  * message strings into digestable maps
  *
@@ -42,9 +32,7 @@ module.exports = class MessageParser {
     const rawMessages = message.split(C.MESSAGE_SEPERATOR)
 
     for (let i = 0; i < rawMessages.length; i++) {
-      if (rawMessages[i].length > 2) {
-        callback(this.parseMessage(rawMessages[i]), message)
-      }
+      callback(this.parseMessage(rawMessages[i]), message)
     }
   }
 
@@ -60,16 +48,7 @@ module.exports = class MessageParser {
    */
   static parseMessage (message) {
     const parts = message.split(C.MESSAGE_PART_SEPERATOR)
-
-    if (parts.length < 2) {
-      return null
-    }
-
-    if (!actions.has(parts[1])) {
-      return null
-    }
-
-    return {
+    return parts.length < 2 ? null : {
       raw: message,
       topic: parts[0],
       action: parts[1],
