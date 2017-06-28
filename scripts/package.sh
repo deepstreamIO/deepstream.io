98102a403dbeb44d219704d0519c0d89eb372ac9
Yasser Fadl <yasser.fadl@gmail.com>
2017-06-28 13:04:53 +0200

    style: changing package.sh styling

--- scripts/package.sh
+++ scripts/package.sh
@@ -18,7 +18,7 @@ UWS_SOURCE="nexe_node/uWebSockets/"
 
 EXTENSION=""
 if [ $OS = "win32" ]; then
-	EXTENSION=".exe"
+    EXTENSION=".exe"
 fi
 EXECUTABLE_NAME="build/deepstream$EXTENSION"
 
@@ -30,30 +30,30 @@ echo "Installing missing npm packages, just in case something changes"
 npm i
 
 if ! [[ $NODE_VERSION_WITHOUT_V == $LTS_VERSION* ]]; then
-	echo "Packaging only done on $LTS_VERSION.x"
-	exit
+    echo "Packaging only done on $LTS_VERSION.x"
+    exit
 fi
 
 if [ -z $1  ]; then
-	if ! [[ ${TRAVIS_BRANCH} = 'master' ]] && ! [[ ${APPVEYOR_REPO_BRANCH} = 'master' ]] && ! [[ ${GIT_BRANCH} = 'master' ]]; then
-		echo "Running on branch ${GIT_BRANCH}"
-		if [[ -z ${TRAVIS_TAG} ]] && [[ -z ${APPVEYOR_REPO_TAG} ]]; then
-			echo "Only runs on tags or master"
-			exit
-		elif [[ ${APPVEYOR_REPO_TAG} = false ]]; then
-			echo "On appveyor, not a tag or master"
-			exit
-		else
-			echo "Running on tag ${TRAVIS_TAG} ${APPVEYOR_REPO_TAG}"
-		fi
-	else
-		echo "Running on master"
-	fi
+    if ! [[ ${TRAVIS_BRANCH} = 'master' ]] && ! [[ ${APPVEYOR_REPO_BRANCH} = 'master' ]] && ! [[ ${GIT_BRANCH} = 'master' ]]; then
+        echo "Running on branch ${GIT_BRANCH}"
+        if [[ -z ${TRAVIS_TAG} ]] && [[ -z ${APPVEYOR_REPO_TAG} ]]; then
+            echo "Only runs on tags or master"
+            exit
+        elif [[ ${APPVEYOR_REPO_TAG} = false ]]; then
+            echo "On appveyor, not a tag or master"
+            exit
+        else
+            echo "Running on tag ${TRAVIS_TAG} ${APPVEYOR_REPO_TAG}"
+        fi
+    else
+        echo "Running on master"
+    fi
 fi
 
 if [ $OS = "linux" ]; then
-	echo "Checking if FPM is installed"
-	fpm --version
+    echo "Checking if FPM is installed"
+    fpm --version
 fi
 
 echo -e "\tGenerate License File using unmodified npm packages"
@@ -67,8 +67,8 @@ mkdir -p nexe_node/node/$NODE_VERSION_WITHOUT_V
 cd nexe_node/node/$NODE_VERSION_WITHOUT_V
 rm -rf node-$NODE_VERSION_WITHOUT_V
 if [ ! -f node-$NODE_VERSION_WITHOUT_V.tar.gz ]; then
-	echo -e "\tDownloading node src"
-	curl -o node-$NODE_VERSION_WITHOUT_V.tar.gz https://nodejs.org/dist/v$NODE_VERSION_WITHOUT_V/node-v$NODE_VERSION_WITHOUT_V.tar.gz
+    echo -e "\tDownloading node src"
+    curl -o node-$NODE_VERSION_WITHOUT_V.tar.gz https://nodejs.org/dist/v$NODE_VERSION_WITHOUT_V/node-v$NODE_VERSION_WITHOUT_V.tar.gz
 fi
 
 echo -e "\tUnpacking node"
@@ -92,15 +92,15 @@ echo -e "\t\tAdding UWS into node"
 C_FILE_NAMES="      'src\/uws\/extension.cpp', 'src\/uws\/Extensions.cpp', 'src\/uws\/Group.cpp', 'src\/uws\/WebSocketImpl.cpp', 'src\/uws\/Networking.cpp', 'src\/uws\/Hub.cpp', 'src\/uws\/uws_Node.cpp', 'src\/uws\/WebSocket.cpp', 'src\/uws\/HTTPSocket.cpp', 'src\/uws\/Socket.cpp',"
 
 if [ $OS = "darwin" ]; then
-	echo -e "\t\tapplying patches only tested on darwin node v6.9.1"
-	sed -i '' "s@'library_files': \[@'library_files': \[ 'lib\/uws.js',@" $NODE_SOURCE/node.gyp
-	sed -i '' "s@'src/debug-agent.cc',@'src\/debug-agent.cc',$C_FILE_NAMES@" $NODE_SOURCE/node.gyp
-	sed -i '' "s@'CLANG_CXX_LANGUAGE_STANDARD': 'gnu++0x',  # -std=gnu++0x@'CLANG_CXX_LANGUAGE_STANDARD': 'gnu++0x', 'CLANG_CXX_LIBRARY': 'libc++',@" $NODE_SOURCE/common.gypi
-	sed -i '' "14,18d" $NODE_SOURCE/src/util.h
+    echo -e "\t\tapplying patches only tested on darwin node v6.9.1"
+    sed -i '' "s@'library_files': \[@'library_files': \[ 'lib\/uws.js',@" $NODE_SOURCE/node.gyp
+    sed -i '' "s@'src/debug-agent.cc',@'src\/debug-agent.cc',$C_FILE_NAMES@" $NODE_SOURCE/node.gyp
+    sed -i '' "s@'CLANG_CXX_LANGUAGE_STANDARD': 'gnu++0x',  # -std=gnu++0x@'CLANG_CXX_LANGUAGE_STANDARD': 'gnu++0x', 'CLANG_CXX_LIBRARY': 'libc++',@" $NODE_SOURCE/common.gypi
+    sed -i '' "14,18d" $NODE_SOURCE/src/util.h
 else
-	sed -i "s/'library_files': \[/'library_files': \[\n      'lib\/uws.js',/" $NODE_SOURCE/node.gyp
-	sed -i "s/'src\/debug-agent.cc',/'src\/debug-agent.cc',\n  $C_FILE_NAMES/" $NODE_SOURCE/node.gyp
-	sed -i "s/} catch (e) {/} catch (e) { console.log( e );/" $UWS_SOURCE/nodejs/dist/uws.js
+    sed -i "s/'library_files': \[/'library_files': \[\n      'lib\/uws.js',/" $NODE_SOURCE/node.gyp
+    sed -i "s/'src\/debug-agent.cc',/'src\/debug-agent.cc',\n  $C_FILE_NAMES/" $NODE_SOURCE/node.gyp
+    sed -i "s/} catch (e) {/} catch (e) { console.log( e );/" $UWS_SOURCE/nodejs/dist/uws.js
 fi
 
 mkdir -p $NODE_SOURCE/src/uws
@@ -111,20 +111,20 @@ cp $UWS_SOURCE/nodejs/addon.h $NODE_SOURCE/src/uws
 cp $UWS_SOURCE/nodejs/dist/uws.js $NODE_SOURCE/lib/uws.js
 
 if [ $OS = "win32" ]; then
-	echo "Windows icon"
+    echo "Windows icon"
 
-	NAME=$PACKAGE_VERSION
+    NAME=$PACKAGE_VERSION
 
-	echo -e "\tPatch the window executable icon and details"
-	cp scripts/resources/node.rc $NODE_SOURCE/src/res/node.rc
-	cp scripts/resources/deepstream.ico $NODE_SOURCE/src/res/deepstream.ico
+    echo -e "\tPatch the window executable icon and details"
+    cp scripts/resources/node.rc $NODE_SOURCE/src/res/node.rc
+    cp scripts/resources/deepstream.ico $NODE_SOURCE/src/res/deepstream.ico
 
-	if ! [[ $PACKAGE_VERSION =~ ^[0-9]+[.][0-9]+[.][0-9]+$ ]]; then
-		echo -e "\tVersion can't contain characters in MSBuild, so replacing $PACKAGE_VERSION with 0.0.0"
-		NAME="0.0.0"
-	fi
+    if ! [[ $PACKAGE_VERSION =~ ^[0-9]+[.][0-9]+[.][0-9]+$ ]]; then
+        echo -e "\tVersion can't contain characters in MSBuild, so replacing $PACKAGE_VERSION with 0.0.0"
+        NAME="0.0.0"
+    fi
 
-	sed -i "s/DEEPSTREAM_VERSION/$NAME/" $NODE_SOURCE/src/res/node.rc
+    sed -i "s/DEEPSTREAM_VERSION/$NAME/" $NODE_SOURCE/src/res/node.rc
 fi
 
 # Nexe Patches
@@ -158,17 +158,17 @@ NODE_VERSION_WITHOUT_V=$NODE_VERSION_WITHOUT_V EXECUTABLE_NAME=$EXECUTABLE_NAME 
 PROC_ID=$!
 SECONDS=0;
 while kill -0 "$PROC_ID" >/dev/null 2>&1; do
-	echo -ne "\rCompiling deepstream... ($SECONDS SECONDS)"
-	sleep 1
+    echo -ne "\rCompiling deepstream... ($SECONDS SECONDS)"
+    sleep 1
 done
 
 echo ""
 
 if wait $pid; then
-	echo -e "\tNexe Build Succeeded"
+    echo -e "\tNexe Build Succeeded"
 else
-	echo -e "\tNexe Build Failed"
-	exit 1
+    echo -e "\tNexe Build Failed"
+    exit 1
 fi
 
 echo "Adding docs"
@@ -189,148 +189,148 @@ echo "Patching config file for zip lib and var directories"
 cp -f ./scripts/package-conf.yml $DEEPSTREAM_PACKAGE/conf/config.yml
 
 if [ $OS = "win32" ]; then
-	COMMIT_NAME="deepstream.io-windows-$PACKAGE_VERSION-$COMMIT.zip "
-	CLEAN_NAME="deepstream.io-windows-$PACKAGE_VERSION.zip"
+    COMMIT_NAME="deepstream.io-windows-$PACKAGE_VERSION-$COMMIT.zip "
+    CLEAN_NAME="deepstream.io-windows-$PACKAGE_VERSION.zip"
 
-	echo "OS is windows"
-	echo -e "\tCreating zip deepstream.io-windows-$PACKAGE_VERSION.zip"
-	cd $DEEPSTREAM_PACKAGE
-	7z a ../$COMMIT_NAME . > /dev/null
-	cp ../$COMMIT_NAME ../../$CLEAN_NAME
-	cd -
+    echo "OS is windows"
+    echo -e "\tCreating zip deepstream.io-windows-$PACKAGE_VERSION.zip"
+    cd $DEEPSTREAM_PACKAGE
+    7z a ../$COMMIT_NAME . > /dev/null
+    cp ../$COMMIT_NAME ../../$CLEAN_NAME
+    cd -
 fi
 
 if [ $OS = "darwin" ]; then
-	COMMIT_NAME="deepstream.io-mac-$PACKAGE_VERSION-$COMMIT.zip"
-	CLEAN_NAME="deepstream.io-mac-$PACKAGE_VERSION.zip"
+    COMMIT_NAME="deepstream.io-mac-$PACKAGE_VERSION-$COMMIT.zip"
+    CLEAN_NAME="deepstream.io-mac-$PACKAGE_VERSION.zip"
 
-	echo "OS is mac"
-	echo -e "\tCreating $CLEAN_NAME"
+    echo "OS is mac"
+    echo -e "\tCreating $CLEAN_NAME"
 
-	cd $DEEPSTREAM_PACKAGE
-	zip -r ../$COMMIT_NAME .
-	cp ../$COMMIT_NAME ../../$CLEAN_NAME
-	cd -
+    cd $DEEPSTREAM_PACKAGE
+    zip -r ../$COMMIT_NAME .
+    cp ../$COMMIT_NAME ../../$CLEAN_NAME
+    cd -
 
-	# Adding ruby dir to path
-	PATH="`ruby -e 'puts Gem.user_dir'`/bin:$PATH"
+    # Adding ruby dir to path
+    PATH="`ruby -e 'puts Gem.user_dir'`/bin:$PATH"
 
-	if [ -z $TRAVIS ]; then
-	  gem update --system
+    if [ -z $TRAVIS ]; then
+      gem update --system
 
-	  echo "\tInstalling fpm"
-	  gem install fpm --user-install --no-ri --no-rdoc
+      echo "\tInstalling fpm"
+      gem install fpm --user-install --no-ri --no-rdoc
     fi
 
- 	rm -rf build/osxpkg
- 	mkdir -p build/osxpkg/bin
- 	mkdir -p build/osxpkg/etc/deepstream
- 	mkdir -p build/osxpkg/lib/deepstream
- 	mkdir -p build/osxpkg/share/doc/deepstream
- 	mkdir -p build/osxpkg/var/log/deepstream
+    rm -rf build/osxpkg
+    mkdir -p build/osxpkg/bin
+    mkdir -p build/osxpkg/etc/deepstream
+    mkdir -p build/osxpkg/lib/deepstream
+    mkdir -p build/osxpkg/share/doc/deepstream
+    mkdir -p build/osxpkg/var/log/deepstream
 
- 	cp -r ./build/$PACKAGE_VERSION/deepstream.io/deepstream build/osxpkg/bin/deepstream
- 	cp -r ./build/$PACKAGE_VERSION/deepstream.io/conf/* build/osxpkg/etc/deepstream
- 	cp -r ./build/$PACKAGE_VERSION/deepstream.io/lib/* build/osxpkg/lib/deepstream
- 	cp -r ./build/$PACKAGE_VERSION/deepstream.io/doc/* build/osxpkg/share/doc/deepstream
+    cp -r ./build/$PACKAGE_VERSION/deepstream.io/deepstream build/osxpkg/bin/deepstream
+    cp -r ./build/$PACKAGE_VERSION/deepstream.io/conf/* build/osxpkg/etc/deepstream
+    cp -r ./build/$PACKAGE_VERSION/deepstream.io/lib/* build/osxpkg/lib/deepstream
+    cp -r ./build/$PACKAGE_VERSION/deepstream.io/doc/* build/osxpkg/share/doc/deepstream
 
-	echo "Patching config file for lib and var directories"
-	sed -i '' 's@ ../lib@ /usr/local/lib/deepstream@' build/osxpkg/etc/deepstream/config.yml
-	sed -i '' 's@ ../var@ /usr/local/var/log/deepstream@' build/osxpkg/etc/deepstream/config.yml
+    echo "Patching config file for lib and var directories"
+    sed -i '' 's@ ../lib@ /usr/local/lib/deepstream@' build/osxpkg/etc/deepstream/config.yml
+    sed -i '' 's@ ../var@ /usr/local/var/log/deepstream@' build/osxpkg/etc/deepstream/config.yml
 
- 	cp build/osxpkg/etc/deepstream/config.yml build/osxpkg/etc/deepstream/config.defaults
+    cp build/osxpkg/etc/deepstream/config.yml build/osxpkg/etc/deepstream/config.defaults
 
- 	echo "\tTemporary OSXPKG directory hacks"
- 	mv -f build/osxpkg local
+    echo "\tTemporary OSXPKG directory hacks"
+    mv -f build/osxpkg local
 
-	echo "\tCreating *.pkg"
-	fpm \
-	 	-s dir \
-	 	-t osxpkg \
-		--prefix usr \
-	 	-v $PACKAGE_VERSION \
-	 	--package build \
-	 	-n deepstream.io \
-	 	--license "AGPL-3.0" \
-	 	--vendor "deepstreamHub GmbH" \
-	 	--url https://deepstream.io/ \
-	 	-m "<info@deepstreamhub.com>" \
-	 	local
+    echo "\tCreating *.pkg"
+    fpm \
+        -s dir \
+        -t osxpkg \
+        --prefix usr \
+        -v $PACKAGE_VERSION \
+        --package build \
+        -n deepstream.io \
+        --license "AGPL-3.0" \
+        --vendor "deepstreamHub GmbH" \
+        --url https://deepstream.io/ \
+        -m "<info@deepstreamhub.com>" \
+        local
 
- 	echo "\tDeleting OSXPKG Temporary directory hacks"
-	rm -rf local
+    echo "\tDeleting OSXPKG Temporary directory hacks"
+    rm -rf local
 fi
 
 if [ $OS = "linux" ]; then
-	echo "OS is linux"
+    echo "OS is linux"
 
-	echo -e "\tCreating tar.gz"
+    echo -e "\tCreating tar.gz"
 
-	COMMIT_NAME="deepstream.io-linux-$PACKAGE_VERSION-$COMMIT.tar.gz"
-	CLEAN_NAME="deepstream.io-linux-$PACKAGE_VERSION.tar.gz"
+    COMMIT_NAME="deepstream.io-linux-$PACKAGE_VERSION-$COMMIT.tar.gz"
+    CLEAN_NAME="deepstream.io-linux-$PACKAGE_VERSION.tar.gz"
 
-	cd $DEEPSTREAM_PACKAGE
-	tar czf ../$COMMIT_NAME .
-	cp ../$COMMIT_NAME ../../$CLEAN_NAME
-	cd -
+    cd $DEEPSTREAM_PACKAGE
+    tar czf ../$COMMIT_NAME .
+    cp ../$COMMIT_NAME ../../$CLEAN_NAME
+    cd -
 
-	echo -e "\tPatching config file for linux distros..."
+    echo -e "\tPatching config file for linux distros..."
 
-	if [ $OS = "darwin" ]; then
-		sed -i '' 's@ ../lib@ /var/lib/deepstream@' $DEEPSTREAM_PACKAGE/conf/config.yml
-		sed -i '' 's@ ../var@ /var/log/deepstream@' $DEEPSTREAM_PACKAGE/conf/config.yml
-	else
-		sed -i 's@ ../lib@ /var/lib/deepstream@' $DEEPSTREAM_PACKAGE/conf/config.yml
-		sed -i 's@ ../var@ /var/log/deepstream@' $DEEPSTREAM_PACKAGE/conf/config.yml
-	fi
+    if [ $OS = "darwin" ]; then
+        sed -i '' 's@ ../lib@ /var/lib/deepstream@' $DEEPSTREAM_PACKAGE/conf/config.yml
+        sed -i '' 's@ ../var@ /var/log/deepstream@' $DEEPSTREAM_PACKAGE/conf/config.yml
+    else
+        sed -i 's@ ../lib@ /var/lib/deepstream@' $DEEPSTREAM_PACKAGE/conf/config.yml
+        sed -i 's@ ../var@ /var/log/deepstream@' $DEEPSTREAM_PACKAGE/conf/config.yml
+    fi
 
-	echo -e "\t\tCreating rpm"
+    echo -e "\t\tCreating rpm"
 
-	fpm \
-		-s dir \
-		-t rpm \
-		--package ./build/ \
-		--package-name-suffix $COMMIT \
-		-n deepstream.io \
-		-v $PACKAGE_VERSION \
-		--license "AGPL-3.0" \
-		--vendor "deepstreamHub GmbH" \
-		--description "deepstream.io rpm package" \
-		--url https://deepstream.io/ \
-		-m "<info@deepstreamhub.com>" \
-		--after-install ./scripts/daemon/after-install \
-		--before-remove ./scripts/daemon/before-remove \
-		--before-upgrade ./scripts/daemon/before-upgrade \
-		--after-upgrade ./scripts/daemon/after-upgrade \
-		-f \
-		$DEEPSTREAM_PACKAGE/doc/=/usr/share/doc/deepstream/ \
-		$DEEPSTREAM_PACKAGE/conf/=/etc/deepstream/conf.d/ \
-		$DEEPSTREAM_PACKAGE/lib/=/var/lib/deepstream/ \
-		./build/deepstream=/usr/bin/deepstream
+    fpm \
+        -s dir \
+        -t rpm \
+        --package ./build/ \
+        --package-name-suffix $COMMIT \
+        -n deepstream.io \
+        -v $PACKAGE_VERSION \
+        --license "AGPL-3.0" \
+        --vendor "deepstreamHub GmbH" \
+        --description "deepstream.io rpm package" \
+        --url https://deepstream.io/ \
+        -m "<info@deepstreamhub.com>" \
+        --after-install ./scripts/daemon/after-install \
+        --before-remove ./scripts/daemon/before-remove \
+        --before-upgrade ./scripts/daemon/before-upgrade \
+        --after-upgrade ./scripts/daemon/after-upgrade \
+        -f \
+        $DEEPSTREAM_PACKAGE/doc/=/usr/share/doc/deepstream/ \
+        $DEEPSTREAM_PACKAGE/conf/=/etc/deepstream/conf.d/ \
+        $DEEPSTREAM_PACKAGE/lib/=/var/lib/deepstream/ \
+        ./build/deepstream=/usr/bin/deepstream
 
-	echo -e "\t\tCreating deb"
-	fpm \
-		-s dir \
-		-t deb \
-		--package ./build \
-		--package-name-suffix $COMMIT \
-		-n deepstream.io \
-		-v $PACKAGE_VERSION \
-		--license "AGPL-3.0" \
-		--vendor "deepstreamHub GmbH" \
-		--description "deepstream.io deb package" \
-		--url https://deepstream.io/ \
-		-m "<info@deepstreamhub.com>" \
-		--after-install ./scripts/daemon/after-install \
-		--before-remove ./scripts/daemon/before-remove \
-		--before-upgrade ./scripts/daemon/before-upgrade \
-		--after-upgrade ./scripts/daemon/after-upgrade \
-		-f \
-		--deb-no-default-config-files \
-		$DEEPSTREAM_PACKAGE/doc/=/usr/share/doc/deepstream/ \
-		$DEEPSTREAM_PACKAGE/conf/=/etc/deepstream/conf.d/ \
-		$DEEPSTREAM_PACKAGE/lib/=/var/lib/deepstream/ \
-		./build/deepstream=/usr/bin/deepstream
+    echo -e "\t\tCreating deb"
+    fpm \
+        -s dir \
+        -t deb \
+        --package ./build \
+        --package-name-suffix $COMMIT \
+        -n deepstream.io \
+        -v $PACKAGE_VERSION \
+        --license "AGPL-3.0" \
+        --vendor "deepstreamHub GmbH" \
+        --description "deepstream.io deb package" \
+        --url https://deepstream.io/ \
+        -m "<info@deepstreamhub.com>" \
+        --after-install ./scripts/daemon/after-install \
+        --before-remove ./scripts/daemon/before-remove \
+        --before-upgrade ./scripts/daemon/before-upgrade \
+        --after-upgrade ./scripts/daemon/after-upgrade \
+        -f \
+        --deb-no-default-config-files \
+        $DEEPSTREAM_PACKAGE/doc/=/usr/share/doc/deepstream/ \
+        $DEEPSTREAM_PACKAGE/conf/=/etc/deepstream/conf.d/ \
+        $DEEPSTREAM_PACKAGE/lib/=/var/lib/deepstream/ \
+        ./build/deepstream=/usr/bin/deepstream
 fi
 
 rm -rf $DEEPSTREAM_PACKAGE
