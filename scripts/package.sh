28529610e009aefce43cbf1767ebf59dac796fec
John Diamond <johndiamond@gmx.co.uk>
2017-07-17 13:49:03 +0200

    build: replace duplicate def of UV_(WRIT|READ)ABLE in uWS

--- scripts/package.sh
+++ scripts/package.sh
@@ -108,6 +108,8 @@ function compile {
         sed -i "s/'src\/debug-agent.cc',/'src\/debug-agent.cc',\n  $C_FILE_NAMES/" $NODE_SOURCE/node.gyp
         sed -i "s/} catch (e) {/} catch (e) { console.log( e );/" $UWS_SOURCE/nodejs/src/uws.js
         sed -i "s@../src/uWS@uWS@" $UWS_SOURCE/nodejs/src/addon.cpp
+        sed -i "s/UV_WRITABLE/UV_WRITABLE_/" $UWS_SOURCE/uWebSockets/src/*
+        sed -i "s/UV_READABLE/UV_READABLE_/" $UWS_SOURCE/uWebSockets/src/*
     fi
 
     mkdir -p $NODE_SOURCE/src/uws
