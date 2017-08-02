a2e6d4ee1cad2632760c76a20a7fc6c99bb1c230
Robert Nagy <ronag89@gmail.com>
2017-08-02 19:28:13 +0200

    perf(message-processor): avoid Map

--- src/message/message-processor.js
+++ src/message/message-processor.js
@@ -1,49 +1,16 @@
 const MessageQueue = require('./message-queue')
 const C = require('../constants/constants')
 
-/**
- * The MessageProcessor consumes blocks of parsed messages emitted by the
- * ConnectionEndpoint, checks if they are permissioned and - if they
- * are - forwards them.
- *
- * @constructor
- *
- * @param {Object} options deepstream options
- */
+const QUEUE_SYMBOL = Symbol('queue')
+
 module.exports = class MessageProcessor {
   constructor (options) {
     this._options = options
-    this._queues = new Map()
   }
 
-  /**
-   * There will only ever be one consumer of forwarded messages. So rather than using
-   * events - and their performance overhead - the messageProcessor exposes
-   * this method that's expected to be overwritten.
-   *
-   * @param   {SocketWrapper} socketWrapper
-   * @param   {Object} message the parsed message
-   *
-   * @overwrite
-   *
-   * @returns {void}
-   */
   onAuthenticatedMessage (socketWrapper, message) {
   }
 
-  /**
-   * This method is the way the message processor accepts input. It receives arrays
-   * of parsed messages, iterates through them and issues permission requests for
-   * each individual message
-   *
-   * @todo The responses from the permissionHandler might arive in any arbitrary order - order them
-   * @todo Handle permission handler timeouts
-   *
-   * @param   {SocketWrapper} socketWrapper
-   * @param   {Object} message parsed message
-   *
-   * @returns {void}
-   */
   process (socketWrapper, message) {
     if (typeof message !== 'string') {
       console.log(typeof message)
@@ -60,13 +27,14 @@ module.exports = class MessageProcessor {
       return
     }
 
-    let queue = this._queues.get(socketWrapper)
+    let queue = socketWrapper[QUEUE_SYMBOL]
+
     if (!queue) {
       queue = new MessageQueue(this._options, socketWrapper)
       queue.onAuthenticatedMessage = this.onAuthenticatedMessage
-      socketWrapper.once('close', (socketWrapper) => this._queues.delete(socketWrapper))
-      this._queues.set(socketWrapper, queue)
+      socketWrapper[QUEUE_SYMBOL] = queue
     }
+
     queue.process(message)
   }
 }
