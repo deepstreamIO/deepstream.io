7d29e6114235bb5fe2a924cb7e84e3abdbcfd60f
Alex Harley <alexbruceharley@gmail.com>
2017-08-15 19:06:57 +0200

    fix: properly running with both subscriptions and nodes added/removed

--- test-cluster/cluster-soak.js
+++ test-cluster/cluster-soak.js
@@ -2,6 +2,7 @@
 const ClusterNode = require('../src/cluster/cluster-node')
 const utils = require('../src/utils/utils')
 const Redis = require('ioredis')
+const pino = require('pino')
 
 module.exports = function (program) {
   program
@@ -31,9 +32,16 @@ let subscriptionCount = 10
 let nodeCount = 2
 
 let subscriptionRate = 10000
-let nodeRate = 1000
+let nodeRate = 10000
 
 async function createClusterNode (port) {
+  ports.push(port)
+
+  if (globalSeedNodes.indexOf(`localhost:${port}`) === -1) {
+    globalSeedNodes.push(`localhost:${port}`)
+  }
+
+  console.time(`started node on port ${port}`)
   const serverName = port
 
   // insert into set
@@ -50,53 +58,65 @@ async function createClusterNode (port) {
       pingInterval: 1000
     }
   })
-
+  await redis.lpush('nodes', serverName)
   clusters[port] = { serverName, node, states: [] }
   await setupRegistries(clusters[port])
+  console.timeEnd(`started node on port ${port}`)
 }
 
 async function setupRegistries (cluster) {
+  const pipeline = redis.pipeline()
   for (let i = 0; i < stateCount; i++) {
     const topic = `TOPIC_${i}`
     const stateRegistry = cluster.node.getStateRegistry(topic)
     for (let j = 0; j < subscriptionCount; j++) {
       const subscription = utils.getUid()
       stateRegistry.add(subscription)
-      await redis.lpush(topic, subscription)
     }
+    pipeline.lpush(topic, stateRegistry.getAll(cluster.node.serverName))
     cluster.states.push(stateRegistry)
   }
+  await pipeline.exec()
 }
 
-function startStopNodes () {
+async function startStopNodes () {
   const start = getRandomBool()
   if (start) {
-    if (ports.length === nodeCount) {
+    const currentNodeCount = Object.keys(clusters).length
+    if (currentNodeCount === nodeCount) {
       return
     }
-    console.log('>>> starting node')
 
     const lastPort = ports[ports.length - 1]
     const newPort = lastPort + 1
-    ports.push(newPort)
-    localSeedNodes.push(`localhost:${newPort}`)
     createClusterNode(newPort)
   } else {
-    if (ports.length === 1 || localSeedNodes.length === 1) {
+    if (ports.length === 2) {
       return
     }
-    console.log('>>> removing node')
 
     const index = utils.getRandomIntInRange(0, ports.length)
     const port = ports[index]
+    const cluster = clusters[port]
 
-    clusters[port].node.close(() => {})
+    console.log('>>> removing node', port)
 
-    delete clusters[port]
+    for (let i = 0; i < cluster.states.length; i++) {
+      const registry = cluster.states[i]
+      const subscriptions = registry.getAll(port)
+      for (let j = 0; j < subscriptions.length; j++) {
+        await redis.lrem(registry._topic, 0, subscriptions[j])
+      }
+    }
+
+    cluster.node.close(() => {})
+
 
     ports.splice(index, 1)
-    localSeedNodes.splice(localSeedNodes.indexOf(port), 1)
-    // delete states in redis
+    globalSeedNodes.splice(globalSeedNodes.indexOf(`localhost:${port}`), 1)
+
+    delete clusters[port]
+    await redis.lrem('nodes', 0, port)
   }
 }
 
@@ -128,23 +148,24 @@ async function clearRedisState () {
     const topic = `TOPIC_${i}`
     await redis.del(topic)
   }
+  await redis.del('nodes')
 }
 
 async function action () {
   if (this.nodes) {
-    nodeCount = this.nodes
+    nodeCount = Number(this.nodes)
   }
   if (this.subscriptions) {
-    subscriptionCount = this.subscriptions
+    subscriptionCount = Number(this.subscriptions)
   }
   if (this.states) {
-    stateCount = this.states
+    stateCount = Number(this.states)
   }
   if (this.subscriptionRate) {
-    subscriptionRate = this.subscriptionRate
+    subscriptionRate = Number(this.subscriptionRate)
   }
   if (this.nodeRate) {
-    nodeRate = this.nodeRate
+    nodeRate = Number(this.nodeRate)
   }
 
   await clearRedisState()
@@ -155,88 +176,73 @@ async function action () {
     globalSeedNodes = localSeedNodes
   }
 
-  for (let i = 0; i < (nodeCount || 2); i++) {
-    ports.push(3030 + i)
-    localSeedNodes.push(`localhost:${3030 + i}`)
-  }
-
   /**
    * Initialise Cluster
    */
-  for (let i = 0; i < ports.length; i++) {
+  for (let i = 0; i < nodeCount; i++) {
     await createClusterNode(3030 + i)
   }
 
   await sleep(1000)
-  console.log('Nodes initialised')
 
   /**
    * Adding / removing nodes
    */
-  // setInterval(() => {
-  //   startStopNodes()
-  // }, nodeRate)
-
+  setInterval(async () => {
+    await startStopNodes()
+    await sleep(1000)
+    await checkNodeState()
+  }, nodeRate)
 
   /**
    * Adding / removing subscriptions
    */
-  setInterval(() => {
-    addRemoveSubscriptions()
-  }, subscriptionRate)
-
-  /**
-   * Assertions
-   */
-
-  /**
-   * Adding / Removing Subscriptions
-   */
-  setInterval(() => {
-    // assert getAll is the same as redis.getServerNames
-    const clusterSize = Object.keys(clusters).length
-    for (const port in clusters) {
-      if (clusterSize !== clusters[port].node.getAll().length + 1) {
-        console.error(`Invalid amount of nodes on ${port}`)
-      }
-    }
-  }, 5000)
-
-  /**
-   * Subscriptions are valid
-   */
   setInterval(async () => {
-    const errors = []
-    for (let i = 0; i < stateCount; i++) {
-      const topic = `TOPIC_${i}`
-      for (const port in clusters) {
-        const registry = clusters[port].node.getStateRegistry(topic)
-        const redisEntries = await redis.lrange(topic, 0, -1)
-        const registryEntries = registry.getAll()
-        // console.log(port, topic, 'redis', redisEntries.length, 'registry', registryEntries.length)
-        if (redisEntries.length !== registryEntries.length) {
-          errors.push(`server ${port} has invalid subscriptions on topic ${topic} expected ${redisEntries.length} actual ${registryEntries.length}`)
-        }
+    await addRemoveSubscriptions()
+    await checkSubscriptionState()
+  }, subscriptionRate)
+}
 
-        for (let j = 0; j < redisEntries.length; j++) {
-          if (registryEntries.indexOf(redisEntries[j]) === -1) {
-            errors.push(`state inconsistent, redis has ${redisEntries[j]} but state registry ${topic} does not`)
-          }
-        }
+async function checkNodeState () {
+  const redisNodes = await redis.lrange('nodes', 0, -1)
+  for (const port in clusters) {
+    if (clusters[port].node.getAll().length !== redisNodes.length - 1) {
+      console.error(`Invalid amount of nodes on ${port}`)
+    }
+  }
+}
 
-        for (let j = 0; j < registryEntries.length; j++) {
-          if (redisEntries.indexOf(registryEntries[j]) === -1) {
-            errors.push(`state inconsistent, state ${topic} has ${registryEntries[j]} but redis does not`)
-          }
-        }
+async function checkSubscriptionState () {
+  const errors = []
+  for (let i = 0; i < stateCount; i++) {
+    const topic = `TOPIC_${i}`
+    for (const port in clusters) {
+      const registry = clusters[port].node.getStateRegistry(topic)
+      const redisEntries = await redis.llen(topic)
+      const registryEntries = registry.getAll()
+    //   // console.log(port, topic, 'redis', redisEntries.length, 'registry', registryEntries.length)
+      if (redisEntries !== registryEntries.length) {
+        errors.push(`server ${port} has invalid subscriptions on topic ${topic} expected ${redisEntries} actual ${registryEntries.length}`)
       }
+
+      // for (let j = 0; j < redisEntries.length; j++) {
+      //   if (registryEntries.indexOf(redisEntries[j]) === -1) {
+      //     errors.push(`state inconsistent, redis has ${redisEntries[j]} but state registry ${topic} does not`)
+      //   }
+      // }
+
+      // for (let j = 0; j < registryEntries.length; j++) {
+      //   if (redisEntries.indexOf(registryEntries[j]) === -1) {
+      //     errors.push(`state inconsistent, state ${topic} has ${registryEntries[j]} but redis does not`)
+      //   }
+      // }
     }
-    if (errors.length !== 0) {
-      console.error(errors.length, 'errors')
-    } else {
-      console.log('state consistent')
-    }
-  }, 5000)
+  }
+  if (errors.length !== 0) {
+    console.error(errors.length, 'errors')
+  } else {
+    console.log('state consistent')
+  }
 }
 
 function sleep (duration) {
