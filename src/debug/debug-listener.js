a3009d323b1bb282528f9c73d4b9884526a99300
Yan Hülsebusch <yh@dezign.de>
2017-08-28 16:21:06 +0200

    listen record which you receive from clients. => server.listenRecord(pattern, callback)

---
+++ src/debug/debug-listener.js
@@ -0,0 +1,35 @@
+'use strict'
+
+this._data = {};
+
+/**
+ * Listen record which you receive from clients.
+ * 
+ * @param {String} pattern The pattern to match records which you receive from clients.
+ * @param {Callback} callback callback with parameter recordName for name of record and recordData for data from record.
+ *
+ * @public
+ * @returns {void}
+ */
+var listenRecord = function(pattern, callback){
+    setInterval(()=>{
+        if (this._data != null){
+            if (this._data.message.data[0].match(pattern)){
+                callback(this._data.message.data[0], this._data.data);
+            }
+            this._data = null;
+        }
+    },100);
+}
+
+/**
+ * Fetch data from record
+ *
+ * @returns {void}
+ */
+var enter = function(data){
+    this._data = data;
+}
+
+exports.listenRecord = listenRecord;
+exports.enter = enter;
