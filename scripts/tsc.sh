8154457cd284cfd0f1bc1033363ad358e8aa7f26
Yasser Fadl <yasser.fadl@gmail.com>
2019-06-20 18:38:19 +0200

    ci: attempt to fix windows build

--- scripts/tsc.sh
+++ scripts/tsc.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 rm -rf dist
-node ./node_modules/.bin/tsc
+./node_modules/.bin/tsc
 cp ./ascii-logo.txt ./dist/ascii-logo.txt
 cp ./package.json ./dist/package.json
 cp -r conf ./dist/conf
