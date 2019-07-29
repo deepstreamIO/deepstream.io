7d6663024cf6b47dbcff8b83aaf4a10d44d3111d
Yasser Fadl <yasser.fadl@gmail.com>
2019-07-29 13:58:37 +0300

    ci: docker attempt 2

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -5,8 +5,11 @@ PACKAGE_VERSION=$( node scripts/details.js VERSION )
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
+npm i
 npm run tsc
 cd dist
-docker build -t deepstreamio/deepstream.io:${PACKAGE_VERSION} -t deepstreamio/deepstream.io:latest
+
+docker build . -t deepstreamio/deepstream.io:${PACKAGE_VERSION} -t deepstreamio/deepstream.io:latest
 docker push deepstreamio/deepstream.io:${PACKAGE_VERSION} deepstreamio/deepstream.io:latest
 
+cd ../
