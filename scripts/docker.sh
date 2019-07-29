fe342b026460405ce085db422c24411aa96d151b
Yasser Fadl <yasser.fadl@gmail.com>
2019-07-29 13:33:39 +0300

    ci: build docker image on tag

---
+++ scripts/docker.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+PACKAGE_VERSION=$( node scripts/details.js VERSION )
+
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+
+npm run tsc
+cd dist
+docker build -t deepstreamio/deepstream.io:${PACKAGE_VERSION} -t deepstreamio/deepstream.io:latest
+docker push deepstreamio/deepstream.io:${PACKAGE_VERSION} deepstreamio/deepstream.io:latest
+
