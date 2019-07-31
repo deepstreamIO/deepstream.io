1296007acf41500a5364d8671fa52b49f9d49ab3
Yasser Fadl <yasser.fadl@gmail.com>
2019-07-31 19:47:40 +0300

    build: Adding an alpine image with just ws

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -10,7 +10,12 @@ npm run tsc
 cd dist
 
 docker build . -t deepstreamio/deepstream.io:${PACKAGE_VERSION} -t deepstreamio/deepstream.io:latest
-docker push deepstreamio/deepstream.io:${PACKAGE_VERSION} 
+docker push deepstreamio/deepstream.io:${PACKAGE_VERSION}
 docker push deepstreamio/deepstream.io:latest
+
+sed -i 's@node:10@node:10-alpine@' Dockerfile
+docker build . -t deepstreamio/deepstream.io:${PACKAGE_VERSION}-alpine
+docker push deepstreamio/deepstream.io:${PACKAGE_VERSION}-alpine
+cp ../Dockerfile .
 
 cd ../
