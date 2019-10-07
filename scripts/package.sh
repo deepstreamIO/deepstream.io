f1b48ef07737ef0d79fd520e98c0d8cac34333de
Yasser Fadl <yasser.fadl@gmail.com>
2019-10-07 15:44:21 +0200

    chore: cucumber 6 broke the build

--- scripts/package.sh
+++ scripts/package.sh
@@ -61,7 +61,7 @@ function compile {
     mkdir build
 
     echo "Installing missing npm packages, just in case something changes"
-    npm i
+    rm -rf node_modules && npm i
 
     echo "Transpiling"
     npm run tsc
