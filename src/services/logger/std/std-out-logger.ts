516fbfc684737d0b98078a0e5b29b889d4b8e9b9
Yasser Fadl <yasser.fadl@gmail.com>
2020-02-08 12:54:23 +1300

    chore: updating dependencies

--- src/services/logger/std/std-out-logger.ts
+++ src/services/logger/std/std-out-logger.ts
@@ -1,7 +1,6 @@
-import chalk from 'chalk'
+import * as chalk from 'chalk'
 import { DeepstreamPlugin, DeepstreamLogger, DeepstreamServices, DeepstreamConfig, LOG_LEVEL, NamespacedLogger, EVENT, MetaData } from '@deepstream/types'
-
-const EOL = require('os').EOL
+import { EOL } from 'os'
 
 export class StdOutLogger extends DeepstreamPlugin implements DeepstreamLogger {
   public description = 'std out/err'
